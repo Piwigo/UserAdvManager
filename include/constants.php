8019a7e9675dfc4b5422758264ebf0244c5e2d54
Eric <eric-piwigo@users.noreply.github.com>
2011-03-25 17:26:53 +0000

    Renaming SVN directory for UserAdvManager

--- include/constants.php
+++ include/constants.php
@@ -2,5 +2,4 @@
 global $prefixeTable;
 define('USER_CONFIRM_MAIL_TABLE', $prefixeTable.'user_confirm_mail');
 define('USER_LASTVISIT_TABLE', $prefixeTable.'user_lastvisit_check');
-define('UAM_OBSOLETE', 'obsolete.list');
 ?>
