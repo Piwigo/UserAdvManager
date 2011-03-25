f0c219a26c0c79ad4c302c658f9e2a97a9e6dc99
Eric <eric-piwigo@users.noreply.github.com>
2011-03-25 17:26:53 +0000

    Renaming SVN directory for UserAdvManager

--- maintain.inc.php
+++ maintain.inc.php
@@ -1,107 +1,42 @@
 <?php
 
-if(!defined('UAM_PATH'))
-{
-  define('UAM_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
-}
+if(!defined("NBC_UserAdvManager_PATH"))
+  define('NBC_UserAdvManager_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
 
-include_once (UAM_PATH.'include/constants.php');
-include_once (UAM_PATH.'include/functions.inc.php');
-
+include_once (NBC_UserAdvManager_PATH.'include/constants.php');
+include_once (NBC_UserAdvManager_PATH.'include/functions_UserAdvManager.inc.php');
 
 function plugin_install()
 {
 	global $conf;
 	
-  $default1 = array('false','false',-1,-1,-1,'false','false','',-1,'','','false','','false',100,'false','false',10,'Hello [username].
+  $q = '
+INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
+VALUES ("nbc_UserAdvManager","true;false;false;-1;-1;-1;false;false;;-1;;;false;;false;100;false,false,10,Hello.
 	
-This is a reminder because a very long time passed since your last visit on our gallery [mygallery]. If you do not want anymore to use your access account, please let us know by replying to this email. Your account will be deleted.
+This is a reminder because a very long time passed since your last visit on our gallery. If you do not want anymore to use your access account, please let us know by replying to this email. Your account will be deleted.
 
 On receipt of this message and no new visit within 15 days, we would be obliged to automatically delete your account.
 
 Best regards,
 
-The admin of the gallery.','false','false','false','false','false','Sorry [username], your account has been deleted due to a too long time passed since your last visit at [mygallery].','Sorry [username], your account has been deprecated due to a too long time passed since your last visit at [mygallery]. Please, use the following link to revalidate your account.',-1,-1,'Thank you for registering at [mygallery]. Your account has been manually validated by _admin_. You may now log in at _link_to_site_ and make any appropriate changes to your profile. Welcome to _name_of_site_!','false','You have requested a password reset on our gallery. Please, find below your new connection settings.','false','Sorry, your account has been deleted because you have not validated your registration in requested time. Please, try registration with a valid and non blocked email account.','false');
-
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager"
-;';
-  $count = pwg_db_num_rows(pwg_query($query));
-  
-  if ($count == 0)
-  {
-    $q = '
-INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
-VALUES ("UserAdvManager","'.addslashes(serialize($default1)).'","UAM parameters")
+The admin of the gallery.","Parametres du plugin nbc UserAdvManager")
   ;';
-    pwg_query($q);
-  }
+  pwg_query($q);
 
-
-  $default2 = array('false',5,'Hello [username].
+  $q = '
+INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
+VALUES ("nbc_UserAdvManager_ConfirmMail","false;5;Hello.
 		
-This is a reminder message because you registered on our gallery [mygallery] but you do not validate your registration and your validation key has expired. To still allow you to access to our gallery, your validation period has been reset. You have again 5 days to validate your registration.
+This is a reminder message because you registered on our gallery but you do not validate your registration and your validation key has expired. To still allow you to access to our gallery, your validation period has been reset. You have again 5 days to validate your registration.
 
-Note: After this period, your account will be permanently deleted.','false','Hello [username].
+Note: After this period, your account will be permanently deleted.;false;Hello.
 
-This is a reminder message because you registered on our gallery [mygallery] but you do not validate your registration and your validation key will expire. To allow you access to our gallery, you have 2 days to confirm your registration by clicking on the link in the message you should have received when you registered.
+This is a reminder message because you registered on our gallery but you do not validate your registration and your validation key will expire. To allow you access to our gallery, you have 2 days to confirm your registration by clicking on the link in the message you should have received when you registered.
 
-Note: After this period, your account will be permanently deleted.','You have confirmed that you are human and may now use [mygallery]! Welcome [username]!','Your activation key is incorrect or expired or you have already validated your account, please contact the webmaster to fix this problem.');
-
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager_ConfirmMail"
-;';
-  $count = pwg_db_num_rows(pwg_query($query));
-  
-  if ($count == 0)
-  {
-    $q = '
-INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
-VALUES ("UserAdvManager_ConfirmMail","'.addslashes(serialize($default2)).'","UAM ConfirmMail parameters")
+Note: After this period, your account will be permanently deleted.","Parametres nbc_UserAdvManager - ConfirmMail")
   ;';
-    pwg_query($q);
-  }
-
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager_Redir"
-;';
-  $count = pwg_db_num_rows(pwg_query($query));
-  
-  if ($count == 0)
-  {
-    $q = '
-INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
-VALUES ("UserAdvManager_Redir","0","UAM Redirections")
-  ;';
-    pwg_query($q);
-  }
-
-// Set current plugin version in config table
-  $plugin =  PluginInfos(UAM_PATH);
-  $version = $plugin['version'];
-
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager_Redir"
-;';
-  $count = pwg_db_num_rows(pwg_query($query));
-  
-  if ($count == 0)
-  {
-    $q = '
-INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
-VALUES ("UserAdvManager_Version","'.$version.'","UAM version check")
-  ;';
-    pwg_query($q);
-  }
-
+  pwg_query($q);
 
 	$q = "
 CREATE TABLE IF NOT EXISTS ".USER_CONFIRM_MAIL_TABLE." (
@@ -110,10 +45,9 @@ CREATE TABLE IF NOT EXISTS ".USER_CONFIRM_MAIL_TABLE." (
   mail_address varchar(255) default NULL,
   status enum('webmaster','admin','normal','generic','guest') default NULL,
   date_check datetime default NULL,
-  reminder ENUM('true','false') NULL,
 PRIMARY KEY  (id)
   )
-ENGINE=MyISAM;";
+;";
   pwg_query($q);
 
 	$q = "
@@ -123,142 +57,34 @@ CREATE TABLE IF NOT EXISTS ".USER_LASTVISIT_TABLE." (
   reminder ENUM('true','false') NULL,
 PRIMARY KEY (`user_id`)
   )
-ENGINE=MyISAM;";
+;";
   pwg_query($q);
 }
 
-
 function plugin_activate()
 {
-  global $conf;
-
-/* Cleaning obsolete files */
-/* *********************** */
-  clean_obsolete_files();
-  
-  include_once (UAM_PATH.'include/upgradedb.inc.php');
-
-/* Check if old version is < 2.15 */
-/* ****************************** */
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "nbc_UserAdvManager"
-;';
-  $count1 = pwg_db_num_rows(pwg_query($query));
-  
-  $query = '
+	  global $conf;
+	
+/* Check for upgrade from 2.10 */
+	  $query = '
 SELECT *
-  FROM '.CONFIG_TABLE.'
+FROM '.CONFIG_TABLE.'
 WHERE param = "nbc_UserAdvManager_ConfirmMail"
 ;';
-  $count2 = pwg_db_num_rows(pwg_query($query)); 
-
-/* If old params exist an upgrade is needed */
-/* **************************************** */
-  if ($count1 == 1)
-  {
-/* Check for upgrade from 2.10 to 2.11 */
-/* *********************************** */
-    if ($count1 == 1 and $count2 == 0)
-    {
-    /* upgrade from branch 2.10 to 2.11 */
-    /* ******************************** */
-      upgrade_210_211();
-    }
-
-
-/* Check for upgrade from 2.11 to 2.12 */
-/* *********************************** */
-    if (!table_exist(USER_LASTVISIT_TABLE))
-    {
-    /* upgrade from branch 2.11 to 2.12 */
-    /* ******************************** */
-  		upgrade_211_212();
-    }
-
-
-/* Check for upgrade from 2.12 to 2.13 */
-/* *********************************** */
-    $fields = mysql_list_fields($conf['db_base'],USER_CONFIRM_MAIL_TABLE);
-    $nb_fields = mysql_num_fields($fields); 
-
-    if ($nb_fields < 6)
-    {
-    /* upgrade from branch 2.12 to 2.13 */
-    /* ******************************** */
-      upgrade_212_213();
-    }
-
-
-/* Serializing conf parameters - Available since 2.14.0 */
-/* **************************************************** */
-    if (unserialize($conf['nbc_UserAdvManager']) === false)
-    {
-    /* upgrade from branch 2.13 to 2.14 */
-    /* ******************************** */
-      upgrade_213_214();
-    }
-    
-
-/* Check for upgrade from 2.14 to 2.15 */
-/* *********************************** */
-    //if ($count1 == 1 or $count2 == 1)
-    //{
-    /* upgrade from branch 2.14 to 2.15 */
-    /* ******************************** */
-      upgrade_214_215();
-    //}
-  }
-
-/* Old version is > 2.15 */
-/* ********************* */
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager_Redir"
-;';
-  $count = pwg_db_num_rows(pwg_query($query));
+  $count = mysql_num_rows(pwg_query($query)); 
   
-  if ($count == 0)
+  /* upgrade from branch 2.10 */
+	if ($count == 0)
+	{
+		upgrade_210();
+	}
+
+/* Check for upgrade from 2.11 */
+  if (!table_exist(USER_LASTVISIT_TABLE))
   {
-    upgrade_2153_2154();
+    /* upgrade from branch 2.11 */
+		upgrade_211();
   }
-
-/* Check for upgrade from 2.15 to 2.16 */
-/* *********************************** */
-	$query = '
-SELECT param
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager_Version"
-;';
-  $count = pwg_db_num_rows(pwg_query($query));
-  
-  if ($count == 0)
-  {
-    /* upgrade from branch 2.15 to 2.16 */
-    /* ******************************** */
-    upgrade_215_2160();
-  }
-
-/* Check for upgrade from 2.16 to 2.20 */
-/* *********************************** */
-	$query = '
-SELECT value
-  FROM '.CONFIG_TABLE.'
-WHERE param = "UserAdvManager_Version"
-;';
-
-  $data = pwg_db_fetch_assoc(pwg_query($query));
-  
-  if ($data['value'] != '2.20.0')
-  {
-    /* upgrade from branch 2.16 to 2.20 */
-    /* ******************************** */
-    upgrade_216_220();
-  }
-
-load_conf_from_db('param like \'UserAdvManager\\_%\'');
 }
 
 
@@ -266,41 +92,21 @@ function plugin_uninstall()
 {
   global $conf;
 
-  if (isset($conf['UserAdvManager']))
+  if (isset($conf['nbc_UserAdvManager']))
   {
     $q = '
 DELETE FROM '.CONFIG_TABLE.'
-WHERE param="UserAdvManager"
+WHERE param="nbc_UserAdvManager"
 ;';
 
     pwg_query($q);
   }
 
-  if (isset($conf['UserAdvManager_ConfirmMail']))
+  if (isset($conf['nbc_UserAdvManager_ConfirmMail']))
   {
     $q = '
 DELETE FROM '.CONFIG_TABLE.'
-WHERE param="UserAdvManager_ConfirmMail"
-;';
-
-    pwg_query($q);
-  }
-
-  if (isset($conf['UserAdvManager_Redir']))
-  {
-    $q = '
-DELETE FROM '.CONFIG_TABLE.'
-WHERE param="UserAdvManager_Redir"
-;';
-
-    pwg_query($q);
-  }
-
-  if (isset($conf['UserAdvManager_Version']))
-  {
-    $q = '
-DELETE FROM '.CONFIG_TABLE.'
-WHERE param="UserAdvManager_Version"
+WHERE param="nbc_UserAdvManager_ConfirmMail"
 ;';
 
     pwg_query($q);
@@ -311,5 +117,66 @@ WHERE param="UserAdvManager_Version"
 
   $q = 'DROP TABLE '.USER_LASTVISIT_TABLE.';';
   pwg_query( $q );
+}
+
+/* upgrade from branch 2.10 */
+function upgrade_210()
+{
+	global $conf;
+	  
+  $q = '
+INSERT INTO '.CONFIG_TABLE.' (param, value, comment)
+VALUES ("nbc_UserAdvManager_ConfirmMail","false;5;Hello.
+		
+This is a reminder message because you registered on our gallery but you do not validate your registration and your validation key has expired. To still allow you to access to our gallery, your validation period has been reset. You have again 5 days to validate your registration.
+
+Note: After this period, your account will be permanently deleted.;false;Hello.
+
+This is a reminder message because you registered on our gallery but you do not validate your registration and your validation key will expire. To allow you access to our gallery, you have 2 days to confirm your registration by clicking on the link in the message you should have received when you registered.
+
+Note: After this period, your account will be permanently deleted.","Parametres nbc_UserAdvManager - ConfirmMail")
+  ;';
+  pwg_query($q);
+
+upgrade_211();
+}
+
+/* upgrade from branch 2.11 */
+function upgrade_211()
+{
+	global $conf;
+
+  $conf_nbc_UserAdvManager = isset($conf['nbc_UserAdvManager']) ? explode(";" , $conf['nbc_UserAdvManager']) : array();
+
+  if ((!isset($conf_nbc_UserAdvManager[14]) and !isset($conf_nbc_UserAdvManager[15])) and !isset($conf_nbc_UserAdvManager[16]) and !isset($conf_nbc_UserAdvManager[17]))
+  {
+    $upgrade_nbc_UserAdvManager = $conf_nbc_UserAdvManager[0].';'.$conf_nbc_UserAdvManager[1].';'.$conf_nbc_UserAdvManager[2].';'.$conf_nbc_UserAdvManager[3].';'.$conf_nbc_UserAdvManager[4].';'.$conf_nbc_UserAdvManager[5].';'.$conf_nbc_UserAdvManager[6].';'.$conf_nbc_UserAdvManager[7].';'.$conf_nbc_UserAdvManager[8].';'.$conf_nbc_UserAdvManager[9].';'.$conf_nbc_UserAdvManager[10].';'.$conf_nbc_UserAdvManager[11].';'.$conf_nbc_UserAdvManager[12].';'.$conf_nbc_UserAdvManager[13].';false;100;false;false;10;Hello.
+	
+This is a reminder because a very long time passed since your last visit on our gallery. If you do not want anymore to use your access account, please let us know by replying to this email. Your account will be deleted.
+
+On receipt of this message and no new visit within 15 days, we would be obliged to automatically delete your account.
+
+Best regards,
+
+The admin of the gallery.';
+		
+		$query = '
+UPDATE '.CONFIG_TABLE.'
+SET value="'.$upgrade_nbc_UserAdvManager.'"
+WHERE param="nbc_UserAdvManager"
+LIMIT 1
+;';
+		pwg_query($query);
+  }
+  
+	$q = "
+CREATE TABLE IF NOT EXISTS ".USER_LASTVISIT_TABLE." (
+  user_id SMALLINT(5) NOT NULL DEFAULT '0',
+  lastvisit DATETIME NULL DEFAULT NULL,
+  reminder ENUM('true','false') NULL,
+PRIMARY KEY (`user_id`)
+  )
+;";
+  pwg_query($q);
 }
 ?>
