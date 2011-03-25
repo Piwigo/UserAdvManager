c6ae37cc208067f0c1a640a121dfff33a74ec210
Eric <eric-piwigo@users.noreply.github.com>
2011-03-25 17:26:53 +0000

    Renaming SVN directory for UserAdvManager

--- changelog.txt.php
+++ changelog.txt.php
@@ -1,10 +1,8 @@
 <?php
 /*
-Plugin Name: UserAdvManager
+Plugin Name: NBC_UserAdvManager
 ** Change log **
-***************************************
 ***** Plugin history (branch 2.10)*****
-***************************************
 
 -- 2.10.0-beta : Initial beta release for Piwigo compatibility
 -- 2.10.1-beta : Small correction on generated path
@@ -68,9 +66,7 @@ Plugin Name: UserAdvManager
 -- 2.11.5 : Bug 1195 fixed : Registration displays the good title
 
 
-***************************************
 ***** Plugin history (branch 2.12)*****
-***************************************
 
 -- 2.12.0 : Bug 1206 fixed : All plugin functionnalities work in user's profile page
             Plugin's core code and admin panel refactoring
@@ -103,9 +99,7 @@ Plugin Name: UserAdvManager
             Escaping all special characters typed in login name and recover them
 
 
-***************************************
 ***** Plugin history (branch 2.13)*****
-***************************************
  
 -- 2.13.0 : Bug 1246 fixed - Extended Description tags are working again ! Caution : The language used and saved in database is the one configured by default in the visitor's browser and not the language given by Language Switch.
             Evolution 1239 - New option to add a new tab that shows the number of days since their last visit for each registered user.
@@ -121,116 +115,6 @@ Plugin Name: UserAdvManager
             Bug 1304 fixed - Adding of plugin version in plugin admin panel title.
 
 -- 2.13.2 : Bug 1308 fixed - Reminder emails have the good translated subject.
-
--- 2.13.3 : Bug 1309 fixed - Forbidden characters in login name work fine again.
-            Bug 1340 fixed - Explanation improvement for option "Nickname is mandatory for comments" 
-            Bug 1342 fixed - Calculation between last visit and today is ok and displays the good color in user list.
-            Italian language improvement (thx to Rio)
-
--- 2.13.4 : Add of obsolete files management
-            Bug 1303 and 1387 fixed - Due to a bug in Piwigo's 2.0.8 switch_lang() function, the email contents using Extended Description tags wasn't taking user's language in account. A first fix is now set for the current 2.0.8 Piwigo's version and another one is ready to work for the next Piwigo's release.
-            Bug 1444 fixed
-            Bug 1445 fixed - The plugin's administration panel have been all reviewed and improved with text simplification and display enhancement.
-            Bug 1463 fixed
-            
-            *** Feature temporarily postponed in a later version due to problems with ";" in text fields *** Add compatibility with FCK Editor plugin for email text fields
-
-
-***************************************
-***** Plugin history (branch 2.14)*****
-***************************************
-
--- 2.14.0 : Bug 1308 refixed - Piwigo 2.0.9 fixes the bug on switch_lang() function so the initial UAM fix is no longer needed 
-            Evolution 1392 - No more confirmation email for admins profile changing 
-            Evolution 1465 - Plugin's configuration data are now serialized in database
-            Bug 1466 fixed - The plugin version is correctly displayed on Ghost Tracker tab
-            Bug 1468 fixed - Java error (thx to cljosse)
-            Evolution 1485 - The admin's can choose if the validation of registration have to be sent to users created by them
-            Improving obsolete files cleaning
-            Evolution 1488 - When an admin creates an account an information email is always sent to created user
-            Code simplification - All variables are changed from "UserAdvManager" to "UAM"
-
--- 2.14.1 : Bug 1497 fixed - Using users tracker without Ghost Tracker is now OK
-
-
-***************************************
-***** Plugin history (branch 2.15)*****
-***************************************
-
--- 2.15.0 : Plugin compatibility for Piwigo 2.1
-            Bug 1467 fixed - FCK Editor's functionnalities are available on registration's confirmation return page customization fields
-            Bug 1474 fixed - Messages on registration's confirmation return page (ConfirmMail.tpl) are customizable 
-            Bug 1508 fixed - Plugin's name is now UserAdvManager (deletion of "nbc_" in code and PEM)
-            Bug 1551 fixed - Database upgrade improvement
-
--- 2.15.1 : Bug 1571 fixed - Missing translation tag
-            Bug 1572 fixed - Fix unable to read resource: "ConfirmMail.tpl"
-            Bug 1574 fixed - Beautifying ConfirmMail page
-            Bug 1576 fixed - Compatibility with other database systems than MySql like PostgreSql or Sqlite. Using Piwigo's pwg_db_### integrated functions.
-            Bug 1586 fixed - Links to official forum topic support and bugtacker were added in plugin's admin page
-
--- 2.15.2 : Bug 1551 re-fixed - There was some problems remaining with old version upgrades
-            Some translations revisited
-            Bug 1655 fixed - Navigation bar is usefull again
-            
--- 2.15.3 : Quick update to fix a database upgrade issue
-
--- 2.15.4 : Bug 1310 fixed - UAM tables are now sortable
-            Bug 1656 fixed - New register validation mode: Manual validation by admin
-            Bug 1687 fixed - Login case sensitivity is no more used in this plugin because already set in Piwigo's core
-            Bug 1727 partially fixed - New option to redirect users to profile page after their first login only.
-              Known problem: The redirection doesn't work after registration and after confirmation page (if ConfirmMail is enabled)
-                             The redirection applies to already registered users including admins, webmaster and generic status.
-            Bug 1789 fixed - Escaping double quotes in text fields
-            Bug 1790 fixed - Validation tracking tab is set when correct options are set
-            Bug 1795 fixed - Fixes rules using email information and/or email of validation
-
--- 2.15.5 : Bug 1693 fixed - Multi-languages are available for ConfirmMail customization (using Extended Description plugin)
-            Bug 1727 fixed - The redirection does not appli to admins, webmaster and generic users.
-            Bug 1807 fixed - Textareas are resized according the screen resolution
-            Bug 1808 fixed - The Tracking users table is ordered by default on "LastVisit" field (last in at top) 
-            Bug 1809 fixed - Addition of a direct link to user's profile in all UAM tables. The link gives a new window
-            Bug 1810 partially fixed - Auto login is not performed after visitors have validated their registration but the "home" button changes his link to redirect to identification page when the redirection option is set. Note: The redirection to profile.php doesn't work because I was unable to use the log_user() function on ConfirmMail page. This feature is still under investigation to perform the best way.
-
--- 2.15.6 : Bug 1819 fixed - Wrong help text on redirection function
-            Bug 1821 fixed - Cleanup of old deprecated functions slags (Case sensitivity on logins)
-            Bug 1834 fixed - Improving plugin installation and uninstallation process
-
--- 2.15.7 : Bug 1869 fixed - Compatibility with Adult_Content installation process
-
--- 2.15.8 : Bug 1935 fixed - Fatal error on ConfirmMail page when Extended Description plugin is not used
-            Bug 1936 fixed - Bad home link in ConfirmMail page when gallery URL is not set
-            small CSS improvement (thx to Gotcha)
-
--- 2.15.9 : Bug 2010 fixed - No confirmation email when information email is not set
-
--- 2.15.10 : Bug 2050 fixed - Compatibility with Captcha
-
-
-***************************************
-***** Plugin history (branch 2.16)*****
-***************************************
--- 2.16.0 : Bug 1585 fixed - UAM version is set in database to improve future upgrades
-            Bug 2011 fixed - Text fields are no longer locked if related option button is not set and saved. Now this fields and unused options are hidden
-            Bug 2046 fixed - Using Piwigo's $conf['insensitive_case_logon'] = true option works again with UAM
-            Bug 2053 fixed - Manual validation by admins wasn't working correctly
-            Bug 2054 fixed - Add of customized email notification to validated users when admins validate them manually 
-            Bug 1430, 1840, 2056 fixed - Automated tasks are available to delete or downgrade ghost users with or without email notification
-            Add of Latvian (lv_LV) translation (Thx to Aivars Baldone)
-
-**************************************************************
-***** Plugin history (branch 2.20 - Piwigo 2.2 compliant)*****
-**************************************************************
--- 2.20.0 : Compliance with Piwigo 2.2
-            Bug 1666 fixed - New feature : Customizing "lost password" email
-            Bug 2045 fixed - New feature : Special tags insertion in text fields. The tags actually available are [username] (insert current user username),[mygallery] (insert current gallery title), [myurl] (insert gallery url if set in Piwigo's configuration options).
-            Bug 2055 fixed - New automated task for unvalidated registers (auto email reminder sent and auto deletion if already reminded).
-            Bug 2072 fixed - Remove sort on "difference in days" in user tracking tab
-            Bug 2140 fixed - English sentence corrections
-            Bug 2186 fixed - JQuery accordion menu when no users are listed in UAM tables
-            Bug 2188 fixed - Avoid translation flags conflicts
-            Bug 2192 fixed - GT Automated tasks improvement and refactory
-            Bug 2203 fixed - [username] special flag is not supported in lost password email customization
 
 */
 ?>
