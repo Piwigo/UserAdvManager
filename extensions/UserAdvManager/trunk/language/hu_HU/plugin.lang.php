d950dc65e50a528fcf4a4a5d98468e5b4529de3d
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-09-07 14:18:54 +0000

    [UserAdvManager] Update Magyar [HU], thanks to : asicu.com

--- extensions/UserAdvManager/trunk/language/hu_HU/plugin.lang.php
+++ extensions/UserAdvManager/trunk/language/hu_HU/plugin.lang.php
@@ -23,11 +23,11 @@ $lang['UAM_confirm_mail_page_title'] = 'Érvényesítsd a regisztrációdat';
 
 /* Errors and Warnings */
 $lang['UAM_audit_ok'] = 'Ellenőrzés rendben, Oké';
-$lang['UAM_Err_audit_username_char'] = '<b>Itt van egy vagy több érvénytelen karakter :</b> ';
+$lang['UAM_Err_audit_username_char'] = '<b>Itt egy vagy több érvénytelen karakter van:</b> ';
 $lang['UAM_Err_audit_email_forbidden'] = '<b>Letiltott e-mail szolgáltatót tartalmaz :</b> ';
-$lang['UAM_Err_audit_advise'] = '<b>új szabályokat állítottál be, amik módosításokat kívánnak.<br>Használj egy adatbázis kezelő programot és javítsd ki a felhasználói adatokat ebben a táblázatban: ###_USERS';
-$lang['UAM_reg_err_login2'] = 'A felhasználónév nem egyezhet meg az következőkkel: ';
-$lang['UAM_reg_err_login5'] = 'Az e-mail szolgáltatód itt le van tiltva.Your email provider is banned for registration. A letiltott szolgáltatók listája: ';
+$lang['UAM_Err_audit_advise'] = '<b>új szabályokat állítottál be, amik módosításokat kívánnak.<br>Használj egy adatbázis-kezelő programot és javítsd ki a felhasználói adatokat ebben a táblázatban: ###_USERS';
+$lang['UAM_reg_err_login2'] = 'A felhasználónév nem egyezhet meg a következőkkel: ';
+$lang['UAM_reg_err_login5'] = 'Az e-mail szolgáltatód itt le van tiltva. A letiltott szolgáltatók listája: ';
 $lang['UAM_empty_pwd'] = '[üres jelszó]';
 $lang['UAM_no_update_pwd'] = '[profil módosítás jelszó változtatás nélkül]';
 $lang['UAM_No_validation_for_Guest'] = 'A "Guest" ("Vendég") azonosítót nem kell érvényesíteni';
@@ -55,19 +55,19 @@ $lang['UAM_Mail_with_key'] = 'Emlékeztető kulccsal';
 
 // --------- Starting below: New or revised $lang ---- from version 2.12.0 and 2.12.1
 /* Global Configuration Tab */
-$lang['UAM_PasswordTest'] = 'Erősség számoló';
+$lang['UAM_PasswordTest'] = 'Jelszó teszt: ';
 /* Ghost Tracker Tab */
 $lang['UAM_Tab_GhostTracker'] = 'Szellem követő';
 $lang['UAM_Reminder'] = 'E-mail emlékeztető';
 $lang['UAM_Reminder_Sent_OK'] = 'IGEN';
 $lang['UAM_Reminder_Sent_NOK'] = 'NEM';
 /* Errors and Warnings */
-$lang['UAM_save_config'] ='Configuráció mentve.';
+$lang['UAM_save_config'] = 'Konfiguráció mentve.';
 $lang['UAM_reg_err_login3'] = 'Biztonság : Jelszó kötelező!';
 $lang['UAM_reg_err_login4_%s'] = 'Biztonság : Egy ellenőrző rendszer kiszámítja a jelszó hatékonyságát. A te jelszavad hatékonysága túlságosan alacsony (hatékonyság = %s). Kérlek, válassz új jelszót az alábbi javaslatok szerint:<br>
-- Használsz betűket és számokat<br>
-- Hazsnálj kis és nagy betűket<br>
-- Növeld a méretét (a jelek számát)<br>
+- Használj betűket és számokat<br>
+- Használj kis és nagy betűket<br>
+- Növeld a jelszavad hosszát (a jelek számát)<br>
 Az admin által megkövetelt minimális hatékonyság: ';
 $lang['UAM_No_reminder_for_Guest'] = 'A "Guest" ("Vendég") azonosító nem kap emlékeztetőt a Szellem-Követőtől';
 $lang['UAM_No_reminder_for_default_user'] = 'Az alapértelmezett azonosító nem kap emlékeztetőt a Szellem-Követőtől';
@@ -89,52 +89,52 @@ $lang['UAM_GT_Reset'] = 'Szellem-Követő visszaállítása';
 
 // --------- Starting below: New or revised $lang ---- from version 2.12.8
 /* Errors and Warnings */
-$lang['UAM_mail_exclusionlist_error'] = 'Warning! You have entered a new line (CR-LF) at the begining of email exclusion list (shown in red below). Although this new line is not visible, it is still present and may cause malfunction of the plugin. Please re-type in your exclusion list in a manner that does not begin with a newline.';
+$lang['UAM_mail_exclusionlist_error'] = 'Figyelem! Egy "új sor"-t (CR-LF) írtál be a kizárandó e-mail-ek listájának elejére (pirossal látható). Bár ez az új sor nem látható, jelen van, és a plugin működésében hibát okozhat. Kérlek, gépeld újra a kizárási listát, hogy ne kezdődjön új sorral.';
 // --------- End: New or revised $lang ---- from version 2.12.8
 
 
 // --------- Starting below: New or revised $lang ---- from version 2.13.0
 /* UserList Tab */
-$lang['UAM_UserList_Title'] = 'Monitoring registered users';
+$lang['UAM_UserList_Title'] = 'Regisztrált felhasználó figyelése';
 // --------- End: New or revised $lang ---- from version 2.13.0
 
 
 // --------- Starting below: New or revised $lang ---- from version 2.13.4
 /* Global Configuration Tab */
-$lang['UAM_Title_Tab'] = 'UserAdvManager - Version : ';
-$lang['UAM_SubTitle1'] = 'Plugin configuration';
-$lang['UAM_Tab_Global'] = 'Configuration';
-$lang['UAM_Title1'] = 'Setting restrictions for registrations';
-$lang['UAM_Title2'] = 'Setting confirmations and validations of registration';
-$lang['UAM_Title3'] = 'Setting the registered users monitoring and other options';
-$lang['UAM_Title4'] = 'Tips and examples of use';
-$lang['UAM_No_Casse'] = 'Usernames: Case sensitivity';
-$lang['UAM_Username_Char'] = 'Usernames: Exclusion of characters';
-$lang['UAM_Username_Char_true'] = ' Banning characters:<br>(Use a comma to separate each character)<br><br>';
-$lang['UAM_Username_Char_false'] = ' Permit all (default)';
-$lang['UAM_Password_Enforced'] = 'Strengthening the security level of passwords';
-$lang['UAM_Password_Enforced_true'] = ' Enable. Minimum Score: ';
-$lang['UAM_AdminPassword_Enforced'] = 'Applying to administrators';
+$lang['UAM_Title_Tab'] = 'UserAdvManager - Verzió : ';
+$lang['UAM_SubTitle1'] = 'Plugin konfiguráció';
+$lang['UAM_Tab_Global'] = 'Konfiguráció';
+$lang['UAM_Title1'] = 'A regisztráció korlátozásainak beállításai';
+$lang['UAM_Title2'] = 'A regisztráció megerősítésének és érvényesítésének beállításai';
+$lang['UAM_Title3'] = 'Regisztrált felhasználók követése és más paraméterek beállítása ';
+$lang['UAM_Title4'] = 'Felhasználási ötletek és példák ';
+$lang['UAM_No_Casse'] = 'Felhasználónév: kis-és nagybetűkre érzékeny';
+$lang['UAM_Username_Char'] = 'Felhasználónév: Kizárandó karakterek';
+$lang['UAM_Username_Char_true'] = ' Kizárandó karakterek:<br>(vesszővel elválasztval)<br><br>';
+$lang['UAM_Username_Char_false'] = ' Mind engedélyezve (alapértelmezett)';
+$lang['UAM_Password_Enforced'] = 'A jelszó biztonságosságának növelése';
+$lang['UAM_Password_Enforced_true'] = ' Érvényesít. Minimális érték: ';
+$lang['UAM_AdminPassword_Enforced'] = 'Adminokra érvényesítés';
 $lang['UAM_PasswordTest'] = 'Password test: ';
-$lang['UAM_ScoreTest'] = 'Result: ';
-$lang['UAM_MailExclusion'] = 'Email domains exclusion';
-$lang['UAM_MailExclusion_true'] = ' Exclude the following domains:<br>(Use a comma to separate each domain)';
+$lang['UAM_ScoreTest'] = 'Eredmény: ';
+$lang['UAM_MailExclusion'] = 'E-mail domainek kizárása';
+$lang['UAM_MailExclusion_true'] = ' Zárd ki a következő domaineket:<br>(Vesszővel elválasztva)';
 
-$lang['UAM_Mail_Info'] = 'Information email to user:';
-$lang['UAM_MailInfo_Text'] = ' Customizing the information email:';
-$lang['UAM_Confirm_Mail'] = 'Confirmation of registration:';
-$lang['UAM_ConfirmMail_Text'] = ' Customizing the confirmation email:';
-$lang['UAM_Confirm_grpstat_notice'] = 'Caution: It is advisable to use either the group or the validation statutes and not both simultaneously.';
-$lang['UAM_Confirm_Group'] = 'Validation Groups<br>(leave ------- to not affect group)';
-$lang['UAM_Confirm_Status'] = 'Validation Statutes<br>(leave ------- to keep the Piwigo\'s default)';
-$lang['UAM_No_Confirm_Group'] = 'Group for users who have not validated their registration<br>';
-$lang['UAM_Validated_Group'] = 'Group for users who have validated their registration<br>';
-$lang['UAM_No_Confirm_Status'] = 'Status for users who have not validated their registration<br>';
-$lang['UAM_Validated_Status'] = 'Status for users who have validated their registration<br>';
-$lang['UAM_ValidationLimit_Info'] = 'Deadline for registration validation limited';
-$lang['UAM_ConfirmMail_TimeOut_true'] = ' Enable. Number of days until expiration: ';
-$lang['UAM_ConfirmMail_Remail'] = 'Remind unvalidated users';
-$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Customizing the reminder message <b><u>with</u></b> new regeneration of key validation.';
+$lang['UAM_Mail_Info'] = 'Info e-mail a felhasználónak:';
+$lang['UAM_MailInfo_Text'] = ' Az info e-mail testreszabása:';
+$lang['UAM_Confirm_Mail'] = 'Regisztráció megerősítése:';
+$lang['UAM_ConfirmMail_Text'] = ' A konfirmációs e-mail testreszabása:';
+$lang['UAM_Confirm_grpstat_notice'] = 'Figyelmeztetés: Célszerű vagy a csoport vagy a hitelesítési alapszabályát használni, és nem a kettőt egyszerre.';
+$lang['UAM_Confirm_Group'] = 'Hitelesítési csoportok<br>(hagyd ------- hogy változatlan maradjon)';
+$lang['UAM_Confirm_Status'] = 'Érvényesítési szabályok<br>(hagyd ------- a Piwigo alapértékeihez)';
+$lang['UAM_No_Confirm_Group'] = 'A regisztrációjukat meg nem erősített felhasználók csoportja<br>';
+$lang['UAM_Validated_Group'] = 'A regisztrációjukat megerősített felhasználók csoportja<br>';
+$lang['UAM_No_Confirm_Status'] = 'A regisztrációjukat meg nem erősített felhasználók állapota<br>';
+$lang['UAM_Validated_Status'] = 'A regisztrációjukat megerősített felhasználók állapota<br>';
+$lang['UAM_ValidationLimit_Info'] = 'A regisztáció megerősítésének határideje korlátozott';
+$lang['UAM_ConfirmMail_TimeOut_true'] = ' Bekapcsol. Napok száma a lejáratig: ';
+$lang['UAM_ConfirmMail_Remail'] = 'Emlékeztető a regisztrációjukat meg nem erősített felhasználóknak ';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = '?????? Az emlékzetető üzenet testreszabása <b><u>with</u></b> new regeneration of key validation.';
 $lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Customizing the reminder message <b><u>without</u></b> regeneration of key validation.';
 
 $lang['UAM_GhostTracker'] = 'Ghost visitors management (Ghost Tracker)';
@@ -187,7 +187,6 @@ $lang['UAM_Err_UserManager_Settings'] = 'This page is available only if "Confirm
 $lang['UAM_Support_txt'] = 'The official support on this plugin is only on these Piwigo forum topic:<br>
 <a href="http://piwigo.org/forum/viewtopic.php?id=15015" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=15015</a><br><br>
 Also available, the project\'s bugtracker: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
-// --------- End: New or revised $lang ---- from version 2.15.1
 
 
 // --------- Starting below: New or revised $lang ---- from version 2.15.4
@@ -207,9 +206,9 @@ $lang['UAM_GTAutoGp'] = 'Automatic change of group / status';
 $lang['UAM_GTAutoMail'] = 'Automatically sending an email when changing group / status';
 $lang['UAM_Deleted_Account_Redirection_Page'] = 'Access denied - Account destroyed!';
 $lang['UAM_title_redir_page'] = 'Access denied for having an account destroyed!';
-$lang['UAM_Error_GTAutoMail_cannot_be_set_without_ConfirmMail'] = 'Consistency error in the chosen configuration:
+$lang['UAM_Error_GTAutoMail_cannot_be_set_without_ConfirmMail'] = 'Consistency error in the configuration chosen:
 <br><br>
-"Setting the registered users monitoring and other options > Ghost visitors management (Ghost Tracker) > Automatic management of ghosts users > Automatically sending an email when changing group / status" can not be activated if "Setting confirmations and validations of registration > Confirmation of registration - Validation by user" is not enabled at first.
+"Setting the followed registrations and other options > Ghost visitors management (Ghost Tracker) > Automatic management of ghosts users > Automatically sending an email when changing group / status" can not be activated if "Setting confirmations and validations of registration > Confirmation of registration - Validation by user" is not enabled at first.
 <br><br>
 To ensure consistency, the option "Automatically sending an email when changing group / status" was automatically repositioned "disabled".
 <br><br>';
@@ -227,8 +226,7 @@ $lang['UAM_USRAutoMail'] = 'Automated email reminder';
 $lang['UAM_Disable'] = ' Disable (default)';
 $lang['UAM_Enable'] = ' Enable ';
 $lang['UAM_Tips1'] = 'Information of non-validated registration with UAM and PWG_Stuffs';
-$lang['UAM_Tips1_txt'] = '
-          <ul>
+$lang['UAM_Tips1_txt'] = '          <ul>
             <li>
             Goals: Inform the visitor that the registration is awaiting approval by displaying a personal block on the home page of the gallery, and this, as registration is not approved.
             <br><br>
@@ -265,12 +263,60 @@ B. In plugin PWG Stuffs :
               </ol>
             </li>
           </ul>';
+<br>
+B. In plugin PWG Stuffs :
+              <ol>
+                <li>Go to tab "Add a new module"</li>
+                <li>Choose "UAM Module"</li>
+                <li>Configure the module, indicating the title (eg "Registration pending validation") and its description, and only check "Waiting" in the list of groups allowed</li>
+                <li>Complete content of the module with the message information to be displayed to users not validated. As UAM, languages markup may be used if the plugin is enabled Extended Description</li>
+                <li>Check "Display the module on the homepage of the site"</li>
+                <li>Validate the configuration of the module</li>
+              </ol>
+            </li>
+          </ul>';
 $lang['UAM_Tips2'] = 'Information of non-validated registration with UAM and Additional Pages';
-$lang['UAM_Tips2_txt'] = '
-          <ul>
+$lang['UAM_Tips2_txt'] = '          <ul>
             <li>
             Goals: Inform the visitor that the registration is awaiting validation by posting an additional page replacing the standard index page gallery at each of these connections, and this, as registration is not approved.
             <br><br>
+            Advantages over the method with PWG_Stuffs: Allow formatting information and displaying the information immediately upon registration of visitors.
+            </li><br><br>
+            <li>
+Prerequisite:<br>
+- A gallery with all or some private categories, visible only by registered users<br>
+- At least 2 following Piwigo\'s users groups: "Waiting," without permission on private categories, and "Validated" with all the permissions on the private categories<br>
+- UAM plugin<br>
+- Additional Pages plugin for adding and managing an additional page to replace the default index page of the gallery<br>
+- Optionally, the plugin Extended Description to support multi-languages<br>
+            </li><br><br>
+            <li>
+Stages:<br><br>
+A. In plugin UAM:
+              <ol>
+                <li>Enable registration confirmation</li>
+                <li>Enter text for additional explanation which will be attached to mail registration confirmation. If the plugin Extended Description is activated, the language tags can be used</li>
+                <li>Select the "Waiting" group under "For users who have not validated their registration"</li>
+                <li>Select the "Validated" group under "For users who have validated their registration"</li>
+                <li>Save the plugin configuration</li>
+              </ol>
+<br>
+B. In plugin Additional Pages:<br>
+                <b>NOTE : The management of access rights for groups on Additional Pages must be turned on (see plugin configuration settings).</b>
+                <br>
+              <ol>
+                <li>Add a new page with at least the following parameters:</li>
+                <ul>
+                  <li>Page name: The name you wish to give to the additional page (ie: Registration not validated)</li>
+                  <li>Set as homepage checked</li>
+                  <li>Groups allowed: Check the box corresponding to the group "Waiting" configured in UAM</li>
+                  <li>Content: The text you want to use for visitors.</li>
+                </ul>
+                <br>
+                <li>And that\'s it! Only visitors registered and whose registration has not been validated will see this additional index page.</li>
+              </ol>
+            </li>
+          </ul>';
             Advantages over the method with PWG_Stuffs: Allow formatting information and displaying the information immediately upon registration of visitors.
             </li><br><br>
             <li>
