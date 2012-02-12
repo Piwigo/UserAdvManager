c9b10d381390e698ec4fc85e9bc685f70ac1fdc4
Eric <Eric@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-02-12 13:38:31 +0000

    Bug 2569 fixed : Adding missing page for users rejected connexions

---
+++ extensions/UserAdvManager/trunk/rejected.php
@@ -0,0 +1,61 @@
+<?php
+//----------------------------------------------------------- include
+define('PHPWG_ROOT_PATH','./../../');
+
+include_once( PHPWG_ROOT_PATH.'include/common.inc.php' );
+
+include_once (UAM_PATH.'include/constants.php');
+include_once (UAM_PATH.'include/functions.inc.php');
+
+$title= l10n('UAM_Rejected_Loggin_Page');
+$page['body_id'] = 'theAboutPage';
+include(PHPWG_ROOT_PATH.'include/page_header.php');
+
+@include(PHPWG_ROOT_PATH.'template/'.$user['template'].
+  '/theme/'.$user['theme'].'/themeconf.inc.php');
+
+
+global $user, $lang, $conf, $errors;
+  
+
+$conf_UAM = unserialize($conf['UserAdvManager']);
+
+if (isset($conf_UAM[40]) and $conf_UAM[40] <> '')
+{
+  // Management of Extension flags ([mygallery], [myurl])
+  // ---------------------------------------------------
+  $patterns[] = '#\[mygallery\]#i';
+  $replacements[] = $conf['gallery_title'];
+  $patterns[] = '#\[myurl\]#i';
+  $replacements[] = $conf['gallery_url'];
+
+  if (function_exists('get_user_language_desc'))
+  {
+    $custom_text = get_user_language_desc(preg_replace($patterns, $replacements, $conf_UAM[40]));
+  }
+  else $custom_text = l10n(preg_replace($patterns, $replacements, $conf_UAM[40]));
+}
+    
+$template->assign(
+  array(
+    'GALLERY_URL'          => make_index_url(),
+    'CUSTOM_REJECT_MSG'    => $custom_text,
+  )
+);
+
+if (isset($lang['Theme: '.$user['theme']]))
+{
+  $template->assign(
+  	'THEME_ABOUT',l10n('Theme: '.$user['theme'])
+  );
+}
+
+$template->set_filenames(
+  array(
+  	'UAM_RejectPage'=>dirname(__FILE__).'/template/rejected.tpl',
+	)
+);
+
+$template->pparse('UAM_RejectPage');
+include(PHPWG_ROOT_PATH.'include/page_tail.php');
+?>
