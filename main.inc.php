bb29212df130ec7f5f8beef5b7c74644dc2ba428
Eric <eric-piwigo@users.noreply.github.com>
2012-06-16 11:40:20 +0000

    merge r15758 from trunk to branch 2.40

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: UserAdvManager
-Version: 2.40.1
+Version: 2.40.2
 Description: Renforcer la gestion des utilisateurs - Enforce users management
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=216
 Author: Nicco, Eric
