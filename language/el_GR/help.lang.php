9856351209b816ebc2f970f3fa604ca95765fbc4
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2012-08-01 15:40:32 +0000

    [useradvmanager] Update el_GR, thanks to :

---
+++ language/el_GR/help.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_AdminValidationMail_Subject_d'] = '
+Όταν o Διαχειριστής ή ο Υπεύθυνος της γκαλερί επιβεβαιώνουν αυτόματη μια αναμονή εγγραφής, στέλνετε αυτόματα ένα email κοινοποίησης στον χρήστη. Εισάγετε εδώ χειροκίνητα το προσαρμοσμένο θέμα για το e-mail επικύρωσης.<br>
+Από προεπιλογή, το πεδίο έχει γενικό κείμενο στην αγγλική γλώσσα αλλα μπορείτε να το αλλάξετε και να χρησιμοποιήσετε τις ετικέτες μετάφρασης [lang =?] του πρόσθετου Extended Description εάν είναι εγκατεστημένο.
+<br><br>
+Οι ακόλουθες ετικέτες πρόσθετων προσαρμογών είναι διαθέσιμες χωρίς να χρειαστεί να χρησιμοποιήσετε το πρόσθετο Extended Description:<br>
+- <b style=&quot;color: red;&quot;>[username]</b> για να εισαγάγετε αυτόματα το όνομα του χρήστη προορισμού του e-mail.<br>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> για να εισάγετε τον τίτλο της γκαλερί σας.<br>';
+?>
---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_CustomPasswRetr'] = 'Προσαρμογή το περιεχόμενο email για χαμένο κωδικό πρόσβασης';
+$lang['UAM_Confirm_grpstat_notice'] = 'Προσοχή: Είναι προτιμότερο να χρησιμοποιήσετε είτε την ομάδα ή την κατάσταση επιβεβαίωσης και όχι και τα δύο ταυτόχρονα. Το επίπεδο προστασίας μπορεί να χρησιμοποιηθεί σε συνδυασμό με μια ομάδα ή την ανάθεση κατάστασης. ';
+$lang['UAM_Confirm_Status'] = 'Κατάσταση<br>(αφήστε ------- να διατηρηθεί το προεπιλεγμένο του Piwigo)';
+$lang['UAM_Confirm_Mail_true'] = 'Ενεργοποίηση. Επιβεβαίωση από τον χρήστη';
+$lang['UAM_Confirm_Mail_local'] = 'Ενεργοποίηση. Επιβεβαίωση από admin (δεν στέλνετε κλειδί επιβεβαίωσης)';
+$lang['UAM_Confirm_Mail'] = 'Επιβεβαίωση της εγγραφής:';
+$lang['UAM_Confirm_Level'] = 'Επίπεδο προστασίας<br>(αφήστε------- να διατηρηθεί το προεπιλεγμένο του Piwigo)';
+$lang['UAM_Confirm_Group'] = 'Ομάδες<br>(αφήστε------- δεν επηρεάζει την ομάδα)';
+$lang['UAM_ConfirmMail_TimeOut_true'] = 'Ενεργοποίηση. Αριθμός Ημερών Μέχρι τη λήξη:';
+$lang['UAM_ConfirmMail_Text'] = 'Προσαρμόστε το e-mail επικύρωσης:';
+$lang['UAM_ConfirmMail_Subject'] = 'Προσαρμόστε το θέμα του e-mail επικύρωσης εγγραφής';
+$lang['UAM_ConfirmMail_Remail'] = 'Υπενθυμίστε τους ανεπιβεβαίωτους χρήστες ';
+$lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Προσαρμογή του μηνύματος υπενθύμισης <b><u>χωρίς</u></b> νέα αναπαραγωγή των κλειδιών επιβεβαίωσης.
+';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Προσαρμογή του μηνύματος υπενθύμισης <b><u>με</u></b> νέα αναπαραγωγή των κλειδιών επιβεβαίωσης.
+';
+$lang['UAM_ConfirmMail_ReMail_Subject'] = 'Προσαρμόστε χειροκίνητα το θέμα του e-mail υπενθύμισης επικύρωσης εγγραφής';
+$lang['UAM_Bad_version_backup'] = 'Η έκδοση του αρχείου backup δεν συμφωνεί με την έκδοση του πρόσθετου! Η αποκατάσταση δεν πραγματοποιήθηκε.';
+$lang['UAM_Admins cannot be pwdreset'] = 'Δεν μπορεί να εφαρμοστεί ανανέωση κωδικού πρόσβασης για λογαριασμούς διαχειριστών';
+$lang['UAM_AdminValidationMail_Text'] = 'Κοινοποίηση της χειροκίνητης επιβεβαίωσης της εγγραφής';
+$lang['UAM_AdminValidationMail_Subject'] = 'Προσαρμόστε χειροκίνητα το θέμα του e-mail επικύρωσης';
+$lang['UAM_AdminPassword_Enforced'] = 'Εφαρμόζετε στους διαχειριστές';
+$lang['UAM_AdminConfMail'] = 'Επιβεβαίωση της εγγραφής για τους admins';
+$lang['UAM_Add of %s'] = 'Προφίλ δημιουργήθηκε για %s ';
+$lang['UAM_%d_Validated_Users'] = '%d Επιβεβαίωση χρηστών χειροκίνητα';
+$lang['UAM_%d_Validated_User'] = '%d Επιβεβαίωση χρήστη χειροκίνητα';
+$lang['UAM_%d_Reminders_Sent'] = '%d μηνύματα υπενθύμισης έχει σταλεί';
+$lang['UAM_%d_Reminder_Sent'] = '%d μήνυμα υπενθύμισης έχει σταλεί';
+$lang['UAM_%d_Mails_With_Key'] = '%d μηνύματα με κλειδιά ανανέωσης έχουν σταλεί';
+$lang['UAM_%d_Mail_With_Key'] = '%d μήνυμα με κλειδί ανανέωσης έχει σταλεί';
+$lang['UAM %d users pwdreseted'] = 'Ανανέωση κωδικού πρόσβασης απαιτείται για τους χρήστες %d';
+$lang['UAM %d user pwdreseted'] = 'Ανανέωση κωδικού πρόσβασης απαιτείται για του χρήστη %d';
+?>
