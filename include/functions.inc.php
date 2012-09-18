24559b7b917321652c77c9b4424e39d7cdde6357
flop25 <flop25@planete-flop.fr>
2012-09-18 15:58:18 +0000

    send mail to admin AND webmasters

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -1180,7 +1180,31 @@ WHERE user_id = '.$id.'
 // -------------------------------------------
 		if (isset($conf_UAM[1]) and $conf_UAM[1] == 'local')
 		{
-  		pwg_mail(get_webmaster_mail_address(), array(
+  		$admins = array();
+      $query = '
+    SELECT
+        u.'.$conf['user_fields']['username'].' AS username,
+        u.'.$conf['user_fields']['email'].' AS mail_address
+      FROM '.USERS_TABLE.' AS u
+        JOIN '.USER_INFOS_TABLE.' AS i ON i.user_id =  u.'.$conf['user_fields']['id'].'
+      WHERE i.status in (\'webmaster\',  \'admin\')
+        AND '.$conf['user_fields']['email'].' IS NOT NULL
+        AND i.user_id <> '.$user['id'].'
+      ORDER BY username
+    ;';
+    
+      $datas = pwg_query($query);
+      if (!empty($datas))
+      {
+        while ($admin = pwg_db_fetch_assoc($datas))
+        {
+          if (!empty($admin['mail_address']))
+          {
+            array_push($admins, format_email($admin['username'], $admin['mail_address']));
+          }
+        }
+      }
+      pwg_mail(implode(', ', $admins), array(
     		'subject' => $subject,
       'content' => (isset($infos1) ? $infos1_perso.l10n_args($infos1)."\n\n" : "").(isset($infos2) ? $infos2_perso.l10n_args($infos2)."\n\n" : "").get_absolute_root_url(),
     ));
