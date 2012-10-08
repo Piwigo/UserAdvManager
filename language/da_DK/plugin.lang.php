a155918f5cd9270946f75c028081261ffc11474d
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2012-10-08 18:39:56 +0000

    [useradvmanager] Update da_DK, thanks to : Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_AdminPassword_Enforced'] = 'Gældende administratorer';
+$lang['UAM_ConfirmMail_Subject'] = 'Tilpas emne på mail vedrørende registreringsbekræftelse';
+$lang['UAM_ConfirmMail_Text'] = ' Tilpasning af bekræftelsesmail:';
+$lang['UAM_ConfirmMail_TimeOut_true'] = ' Aktiver. Antal dage indtil udløb:';
+$lang['UAM_Confirm_Group'] = 'Grupper<br>(bevar ------- for ikke at påvirke gruppe)';
+$lang['UAM_Confirm_Level'] = 'Privatlivsniveau<br>(bevar ------- for at beholde Piwigos standard)';
+$lang['UAM_Confirm_Mail'] = 'Regisreringsbekræftelse:';
+$lang['UAM_Confirm_Mail_local'] = ' Aktiver - Administratorbekræftelse';
+$lang['UAM_Confirm_Mail_true'] = ' Aktiver - Brugerbekræftelse';
+$lang['UAM %d user pwdreseted'] = 'Fornyelse af adgangskode krævet af brugerne %s';
+$lang['UAM %d users pwdreseted'] = 'Fornyelse af adgangskode krævet af brugerne %s';
+$lang['UAM_%d_Mail_With_Key'] = '%s meddelelse med nøglefornyelse afsendt';
+$lang['UAM_%d_Mails_With_Key'] = '%s meddelelser med nøglefornyelse afsendt';
+$lang['UAM_%d_Reminder_Sent'] = '%s påmindelsesmeddelese afsendt';
+$lang['UAM_%d_Reminders_Sent'] = '%s påmindelsesmeddeleser afsendt';
+$lang['UAM_%d_Validated_User'] = '%s bruger bekræftet manuelt';
+$lang['UAM_%d_Validated_Users'] = '%s brugere bekræftet manuelt';
+$lang['UAM_Add of %s'] = 'Profil oprettet for %s';
+$lang['UAM_AdminConfMail'] = 'Registreringsbekræftelse af administratorer';
+$lang['UAM_AdminValidationMail_Subject'] = 'Tilpas emnet på mails vedrørende manuel godkendelse';
+$lang['UAM_AdminValidationMail_Text'] = 'Besked om manuel registreringsbekræftelse';
+$lang['UAM_Admins cannot be pwdreset'] = 'Adgangskodefornyelse kan ikke opsættes på administatorkonto!';
+$lang['UAM_Bad_version_backup'] = 'Versionen af sikkerhedskopien svarer ikke til plugin-versionen!
+Gendannelsen blev ikke udført.';
+$lang['UAM_Change'] = 'Ændring af brugeres gruppe-/status-/privatlivsniveau er bekræftet';
+$lang['UAM_ConfirmMail_ReMail_Subject'] = 'Tilpas emnet på mails vedørende registreringsbekræftelse';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Tilpasning af påmindelsesmeddelelse <b><u>med</u></b> ny gendannelse af bekræftelsesnøgle.';
+$lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Tilpasning af påmindelsesmeddelelse <b><u>uden</u></b> gendannelse af bekræftelsesnøgle.';
+$lang['UAM_ConfirmMail_Remail'] = 'Påmind ikke-bekræftede brugere';
+?>
