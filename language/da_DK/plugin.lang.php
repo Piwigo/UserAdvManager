b05f474e83f6293d342b36401603c6e086c0761c
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2012-10-08 21:34:32 +0000

    [useradvmanager] Update da_DK, thanks to : Kaare

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -49,4 +49,58 @@ $lang['UAM_ConfirmMail_ReMail_Subject'] = 'Tilpas emnet på mails vedørende reg
 $lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Tilpasning af påmindelsesmeddelelse <b><u>med</u></b> ny gendannelse af bekræftelsesnøgle.';
 $lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Tilpasning af påmindelsesmeddelelse <b><u>uden</u></b> gendannelse af bekræftelsesnøgle.';
 $lang['UAM_ConfirmMail_Remail'] = 'Påmind ikke-bekræftede brugere';
+$lang['UAM_Default_InfoMail_Txt'] = 'Hej [username]!
+
+Du har foretaget store ændringer af din profil i galleriet  [mygallery]. Herunder er dine nye indstillinger:';
+$lang['UAM_Default_PwdRequest_Txt'] = 'Du har bedt om nulstilling af adgangskoden til vores galleri. Herunder er dine nye loginoplysninger.';
+$lang['UAM_Default_RejectConnexion_Txt'] = 'Beklager, din registrering er endnu ikke godkendt, så du kan ikke logge på galleriet.';
+$lang['UAM_Default_ValidationTimeout_Txt'] = 'Beklager, din konto er blevet slettet fordi du ikke bekræftede den inden i tide ([Kdays] dage).  Du kan forsøge at registrere dig igen med en gyldig og ikke-blokeret e-mail-adresse.';
+$lang['UAM_Default_InfoMail_Subject'] = '[username] - Dine registreringsoplysninger i [mygallery]';
+$lang['UAM_DumpTxt'] = 'Sikkerhedskopiering og genskabelse af din opsætning';
+$lang['UAM_Dump_Download'] = 'For at downloade sikkerhedskopien sættes flueben i denne boks:';
+$lang['UAM_Dump_NOK'] = 'Fejl: Kunne ikke oprette sikkerhedskopi!';
+$lang['UAM_Dump_OK'] = 'Sikkerhedskopi er oprettet';
+$lang['UAM_Delete_selected'] = 'Slet';
+$lang['UAM_Demotion of %s'] = 'Nedgradering af %s';
+$lang['UAM_Disable'] = ' Deaktiver (standard)';
+$lang['UAM_Enable'] = 'Aktiver';
+$lang['UAM_Confirm_Status'] = 'Statusser<br>(bevar ------- for at beholde Piwigos standard)';
+$lang['UAM_Confirm_grpstat_notice'] = 'Advarsel: Det anbefales at enten benytte grupper eller bekræftelsesstatus, ikke begge på samme tid. Privatlivsniveauet kan benyttes i forbindelse med en gruppe- eller statustildeling.';
+$lang['UAM_CustomPasswRetr'] = 'Tilpas indhold af mail om glemt adgangskode';
+$lang['UAM_Customize_messagesandmails'] = 'Tilpas indhold meddelelserne og mailene';
+$lang['UAM_Default user cannot be pwgreset'] = 'Fornyelse af adgangskode kan ikke opsættes for standardbrugerkontoen!';
+$lang['UAM_Default_AdminValidationMail_Subject'] = '[username] - Registration godkendt i [mygallery]';
+$lang['UAM_Default_AdminValidation_Txt'] = 'Hej [username]! For fordi du registrerede dig og velkommen til [mygallery].
+
+Din konto er blevet manuelt bekræftet af webmaster på [mygallery]. Du kan nu logge på [myurl] og foretage ændringer af din profil.';
+$lang['UAM_Default_CfmMail_Custom_Txt1'] = 'Kontoen [username] blev godkendt!';
+$lang['UAM_Default_CfmMail_Custom_Txt2'] = 'Ukorrekt / udløbet aktiveringsnøgle eller kontoen er allerede godkendt.';
+$lang['UAM_Default_CfmMail_Remail_Txt1'] = 'Hej [username]
+
+Dette er en påmindelse om, at du registrerede dig i vores galleri[mygallery], men ikke bekræftede dette; din bekræftelsesnøgle er nu udløbet. For alligevel at få adgang til vores galleri, er bekræftelsestiden blevet nulstillet. Du har igen x dage til at bekræfte din registrering.
+
+Bemærk: Når nøglen er udløbet, vil kontoen blive slettet permanent.';
+$lang['UAM_Default_CfmMail_Remail_Txt2'] = 'Hej [username]
+
+Dette er en påmindelse om, at du registrerede dig i vores galleri[mygallery], men endnu ikke har bekræftet dette; din bekræftelsesnøgle er ved at udløbe. For at give dig adgang til vores galleri, har du x dage til at bekræfte redistreringen ved at klikke på linket i meddelelsen, som du skulle have modtaget efter din registrering.
+
+Bemærk: Når nøglen er udløbet, vil kontoen blive slettet permanent.';
+$lang['UAM_Default_ConfirmMail_Remail_Subject'] = 'Påmindelse - bekræftelse af registreringen af [username] i [mygallery]';
+$lang['UAM_Default_ConfirmMail_Subject'] = 'Bekræftelse af registrering af [username] i [mygallery]';
+$lang['UAM_Default_ConfirmMail_Txt'] = 'Hej [username]! Tak fordi du registrerede dig i [mygallery].
+
+For at afslutte processen og kunne se billederne, bedes du bekræfte registreringen ved at klikke på linket i denne meddelelse.';
+$lang['UAM_Default_GTAutoMail_Subject'] = '[username] - Din konto er blevet nedgraderet i [mygallery]';
+$lang['UAM_Default_GTReminder_Subject'] = '[username] - Der er gået lang tid siden du sidst besøgte [mygallery]';
+$lang['UAM_Default_GhstDeletion_Txt'] = 'Beklager [username], din konto er blevet slettet fordi der er gået for lang tid siden du sidst besøgte [mygallery]. Du kan registrere dig igen.';
+$lang['UAM_Default_GhstDemotion_Txt'] = 'Beklager [username], din konto er blevet nedlagt fordi der er gået for lang tid siden du sidst besøgte [mygallery]. Benyt venligst følgende link for at genbekræfte din konto:';
+$lang['UAM_Default_GhstReminder_Txt'] = 'Hej [username]
+
+Dette er en påmindelse om, at der er gået lang tid siden du sidst besøgte galleriet [mygallery]. Ønsker du ikke længere at benytte din konto, se giv os besked ved at besvare denne mail. Din konto vil blive slettet.
+
+Ved modtagelse af denne besked og uden nye besøg inden for [days] dage, vil vi automatisk kunne slette din konto.
+
+Med venlig hilsen
+
+Adminstrator af galleriet [mygallery] - [myurl]';
 ?>
