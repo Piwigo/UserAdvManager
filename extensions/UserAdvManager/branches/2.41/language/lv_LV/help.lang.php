1294c27182610169a2ce8c890948464bb2ff5be6
Eric <Eric@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-10-29 19:58:53 +0000

    merge r18799 from trunk : Small admin panel upgrade Update lv_LV, thanks to : agrisans Add da_DK, thanks to : Kaare

--- extensions/UserAdvManager/branches/2.41/language/lv_LV/help.lang.php
+++ extensions/UserAdvManager/branches/2.41/language/lv_LV/help.lang.php
@@ -181,4 +181,20 @@ Tālāk pielāgojiet ziņojuma saturu ar speciāli ievietojamajiem tegiem (iezī
 - <b style=&quot;color: red;&quot;>[myurl]</b> Lai ievadītu jūsu galerijas URL, ja tas iestatīts Piwigo konfigurācijā.
 <br><br>
 Lai lietotu vairākas valodas, ja aktivēts, varat izmantot Paplašinātā apraksta spraudņa tegus.';
+$lang['UAM_ConfirmMail_Subject_d'] = 'Ievadiet šeit savu pielāgoto reģistrācijas apstiprinājuma pasta tēmu. <br>
+Pēc noklusējuma lauks ir vispārējs tekstu angļu valodā, bet to var mainīt un izmantot Paplašinātā Apraksta spraudņa tulkošanas tagus [lang =?], ja uzstādīti.
+<br><br>
+Neizmantojot Paplašinātā Apraksta spraudņa iespējas, pieejami sekojoši papildu pielāgošanas tagi: <br>
+- <b style=&quot;color: red;&quot;>[username]</b> lai automātiski ievietotu e-pasta gala lietotāja vārdu.<br>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> lai ievietotu jūsu galerijas nosaukumu.<br>';
+$lang['UAM_confirmmail_custom1_d'] = 'Kad opcija &quot;Reģistrācijas apstiprināšana&quot; ir aktīva, šis lauks ļauj jums pielāgot <b><u>akceptēšanas tekstu</u></b> reģistrēšanas apstiprināšanas lapā, kas parādās, kad lietotājs uzklikšķina uz pa e-pastā saņemto apstiprināšanas saiti.
+<br><br>
+Pēc spraudņa instalēšanas, standarta teksts tiek iestatīts kā paraugs.
+<br><br>
+Tālāk pielāgojiet saturu ar speciālo tagu-iezīmju palīdzību:<br>
+- <b style=&quot;color: red;&quot;>[username]</b> lai automātiski ievietotu atbilstošo lietotāja vārdu.<br>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> lai ievietotu jūsu galerijas nosaukumu.<br>
+- <b style=&quot;color: red;&quot;>[myurl]</b> lai ievietotu jūsu galerijas URL, ja tas iestatīts Piwigo konfigurācijas opcijās.
+<br><br>
+Šis lauks savietojams ar FCK Redaktoru un, ja lietojat daudzvalodu variantu, ja tas aktīvs, varat izmantot Paplašinātā apraksta spraudņa [lang] tagus.';
 ?>
