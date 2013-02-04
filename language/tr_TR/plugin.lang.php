e3aa2922b30d394bae235889a6838f98fcafb5d6
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2013-02-04 05:37:09 +0000

    [useradvmanager] Update tr_TR, thanks to : hakanselcuk

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -22,4 +22,33 @@
 // +-----------------------------------------------------------------------+
 $lang['UAM_save_config'] = 'Ayarlar Kaydedildi';
 $lang['UAM_submit'] = 'Ayarları Kaydet';
+$lang['UAM_Password: %s'] = 'Şifre: %s';
+$lang['UAM_PasswordTest'] = 'Şifre test:';
+$lang['UAM_No_Casse'] = 'Kullanıcı Adları: Büyük Küçük harf duyarlı';
+$lang['UAM_NewFeature'] = 'Yeni Özellik!';
+$lang['UAM_Restore_File'] = 'Ayarları Geri Yükle';
+$lang['UAM_PwdReset'] = 'Şifre yenileme';
+$lang['UAM_PwdReset_Done'] = 'Şifre yenilendi';
+$lang['UAM_PwdReset_NA'] = 'Orjinal şifre';
+$lang['UAM_PwdReset_Todo'] = 'Şifre yenilemesi';
+$lang['UAM_Registration_Date'] = 'Kayıt tarihi';
+$lang['UAM_Reminder_Sent_NOK'] = 'HAYIR';
+$lang['UAM_Reminder_Sent_OK'] = 'EVET';
+$lang['UAM_Tab_Global'] = 'Yapılandırma';
+$lang['UAM_SubTitle1'] = 'Plugin yapılandırma';
+$lang['UAM_ScoreTest'] = 'Sonuç: ';
+$lang['UAM_Save'] = 'Backup çalıştır';
+$lang['UAM_User: %s'] = 'Kullanıcı : %s';
+$lang['UAM_Update of %s'] = 'Profil %s güncellendi';
+$lang['UAM_no_update_pwd'] = '[Profil şifre değiştirilmeden güncellendi]';
+$lang['UAM_reg_err_login3'] = 'Güvenlik: Şifre zorunludur.';
+$lang['UAM_empty_pwd'] = '[boş şifre]';
+$lang['UAM_confirmmail_custom_Txt1'] = 'Onay sayfası metni - Onay kabul edildi';
+$lang['UAM_confirmmail_custom_Txt2'] = 'Onay sayfası metni - Onay reddedildi';
+$lang['UAM_Default_ConfirmMail_Txt'] = 'Merhaba [username]!  [mygallery] me üye olduğunuz için teşekkür ederim.
+İşleminizi tamamlamak ve fotoğrafları izleyebilmek için mesajdaki linkle tıklayarak kaydınızı tamamlayabilirsiniz.';
+$lang['UAM_%d_Reminder_Sent'] = '%d hatırlatma mesajı gönderildi';
+$lang['UAM_%d_Reminders_Sent'] = '%d hatırlatma mesajları gönderildi';
+$lang['UAM %d users pwdreseted'] = '%d kullanıcıları için şifre yenileme gerekli';
+$lang['UAM %d user pwdreseted'] = '%d kullanıcısı için şifre yenileme gerekli';
 ?>
