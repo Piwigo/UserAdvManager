18060c6b1eb8cd62a08cd6a60d787c4d525a20a2
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-05-03 17:34:13 +0000

    [useradvmanager] Update ru_RU, thanks to : Konve

---
+++ extensions/UserAdvManager/trunk/language/ru_RU/plugin.lang.php
@@ -0,0 +1,71 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_Default_PwdRequest_Txt'] = 'Вы запросили смену пароля для доступа в нашу галерею. Пожалуйста, ознакомьтесь с Вашими новыми настройками для соединения.';
+$lang['UAM_Default_InfoMail_Txt'] = 'Здравствуйте, [username]!
+Вы внесли значительные изменения в Ваш профиль в галерее [mygallery]. Пожалуйста, ознакомьтесь с новыми настройками:';
+$lang['UAM_Default_RejectConnexion_Txt'] = 'Извините, Ваша регистрация еще не завершена, поэтому Вы не можете сейчас войти в галерею.';
+$lang['UAM_Default_ValidationTimeout_Txt'] = 'Извините, Ваш аккаунт был удален, потому что Вы не подтвердили свою регистрацию в требуемое время ([Kdays] дней). Пожалуйста, повторите регистрацию с работающим и незаблокированным электронным адресом.';
+$lang['UAM_Demotion of %s'] = 'Понижение пользователя %s';
+$lang['UAM_Delete_selected'] = 'Удаление';
+$lang['UAM_DumpTxt'] = 'Резервное копирование и восстановление Вашей конфигурации';
+$lang['UAM_Dump_Download'] = 'Для загрузки резервного файла, пожалуйста, поставьте отметку в окне:';
+$lang['UAM_Dump_NOK'] = 'Ошибка: невозможно создать резервный файл!';
+$lang['UAM_Dump_OK'] = 'Резервный файл создан успешно';
+$lang['UAM_Enable'] = 'Включено';
+$lang['UAM_Disable'] = 'Выключено (по умолчанию)';
+$lang['UAM_Default_InfoMail_Subject'] = '[username] - Ваша регистрационная информация в [mygallery]';
+$lang['UAM_Default_GhstReminder_Txt'] = 'Здравствуйте, [username].
+	
+Это письмо-напоминание о том, что прошло очень много времени с Вашего последнего посещения галереи [mygallery]. Если Вы больше не хотите иметь этот аккаунт, пожалуйста, сообщите нам об этом, ответив на этот e-mail. Ваш аккаунт будет удален.
+
+По получении этого сообщения и отсутствии Ваших новых визитов в течение [days] дней, мы автоматически удалим Ваш аккаунт.
+
+С уважением,
+
+Администратор галереии [mygallery] - [myurl]';
+$lang['UAM_Default_GhstDemotion_Txt'] = 'Извините, [username], Ваш аккаунт устарел из-за слишком большого времени, прошедшего с Вашего последнего визита в галерею [mygallery]. Пожалуйста, перейдите по ссылке и подтвердите свой аккаунт:';
+$lang['UAM_Default_GhstDeletion_Txt'] = 'Извините, [username], Ваш аккаунт был удален из-за слишком большого времени, прошедшего с Вашего последнего визита в галерею [mygallery]. Пожалуйста, зарегистрируйтесь снова.';
+$lang['UAM_LastVisit_Date'] = 'Последний визит';
+$lang['UAM_Ghost_reminder_of_%s'] = '%s, это письмо с напоминанием';
+$lang['UAM_reg_err_login5'] = 'С Вашим провайдером эл.почты регистрация недоступна. ';
+$lang['UAM_reg_err_login2'] = 'Имя пользователя не должно содержать следующие символы:';
+$lang['UAM_submit'] = 'Отправка настроек';
+$lang['UAM_save_config'] = 'Конфигурация сохранена.';
+$lang['UAM_reg_err_login3'] = 'Служба безопасности: Пароль обязателен!';
+$lang['UAM_Subject admin validation for %s'] = 'Регистрация пользователя %s ожидает подтверждения';
+$lang['UAM_Title4'] = 'Советы и примеры использования';
+$lang['UAM_Title5'] = 'Прочие функции';
+$lang['UAM_Title2'] = 'Подтверждение регистрации';
+$lang['UAM_Support_txt'] = 'Официальная поддрежка этого плагина только на этом форуме Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=15015" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=15015</a><br><br>
+Еще можете обратиться на бэгтрэкер проекта: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['UAM_SubTitle1'] = 'Конфигурация плагина';
+$lang['UAM_Stuffs_Title'] = 'Блок плагина UAM';
+$lang['UAM_Stuffs'] = 'Блок плагина PWG Stuff';
+$lang['UAM_ScoreTest'] = 'Результат:';
+$lang['UAM_No_validation_for_Guest'] = 'Аккаунт гостя не нуждается в подтверждении';
+$lang['UAM_No_validation_for_Webmaster'] = 'Аккаунт вебмастера не нуждается в подтверждении';
+$lang['UAM_Manual_Validation'] = 'Подтверждение вручную';
+$lang['UAM_Manual_validation_needed_for %s'] = 'Пользователь %s зарегистрирован и ожидает "ручного" подтверждения от администратора.';
+$lang['UAM_MiscOptions'] = 'Другие дополнительные опции';
+?>
