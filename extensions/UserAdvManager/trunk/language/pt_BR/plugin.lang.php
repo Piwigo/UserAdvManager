80c316af9d00ea7ca394925cb3b806b0d923c0dd
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-05-20 05:52:52 +0000

    [useradvmanager] Add pt_BR, thanks to : hespanhol

---
+++ extensions/UserAdvManager/trunk/language/pt_BR/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_%d_Reminders_Sent'] = '%d mensagens de aviso foram enviadas';
+$lang['UAM_%d_Reminder_Sent'] = '%d mensagem de aviso foi enviada';
+$lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Configurar mensagem de aviso <b><u>sem</u></b> geração da chave de confirmação';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Configurar mensagem de aviso <b><u>com</u></b> geração da nova chave de confirmação';
+$lang['UAM_ConfirmMail_Remail'] = 'Avisar usuários não confirmados';
+$lang['UAM_ConfirmMail_ReMail_Subject'] = 'Configurar assunto do email de confirmação de registro';
+$lang['UAM_Change'] = 'Mudar o grupo/status/nível de privacidade dos usuários que foram confirmados';
+$lang['UAM_Bad_version_backup'] = 'A versão do arquivo de backup não é a mesma do plugin! A restauração não foi executada.';
+$lang['UAM_Admins cannot be pwdreset'] = 'Renovação de senha não pode ser feita para contas de administradores!';
+$lang['UAM_AdminValidationMail_Text'] = 'Notificação da confirmação manual de registro';
+$lang['UAM_AdminValidationMail_Subject'] = 'Configurar assunto do email de validação manual';
+$lang['UAM_AdminPassword_Enforced'] = 'Inscrição para administradores';
+$lang['UAM_AdminConfMail'] = 'Confirmação de registro para administradores';
+$lang['UAM_Add of %s'] = 'Perfil criado para %s';
+$lang['UAM_%d_Validated_Users'] = '%d usuários confirmaram manualmente';
+$lang['UAM_%d_Validated_User'] = '%d usuário confirmou manualmente';
+$lang['UAM_%d_Mails_With_Key'] = '%d mensagens com a chave para renovação foram enviadas';
+$lang['UAM_%d_Mail_With_Key'] = '%d mensagem com a chave para renovação foi enviada';
+$lang['UAM %d users pwdreseted'] = 'Obrigatória a renovação da senha para os usuários %d';
+$lang['UAM %d user pwdreseted'] = 'Obrigatória a renovação da senha para o usuário %d';
+?>
