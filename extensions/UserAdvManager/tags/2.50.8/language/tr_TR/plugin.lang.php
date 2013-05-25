9debd1d60df5a6f468586722677618ed4e3a590d
Eric <Eric@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-05-25 13:01:24 +0000

    New tag 2.50.8 from branch 2.50 : Add ru_RU, thanks to : Konve Add pt_BR, thanks to : hespanhol Update tr_TR, thanks to : hakanselcuk

---
+++ extensions/UserAdvManager/tags/2.50.8/language/tr_TR/plugin.lang.php
@@ -0,0 +1,212 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_save_config'] = 'Ayarlar Kaydedildi';
+$lang['UAM_submit'] = 'Ayarları Kaydet';
+$lang['UAM_Password: %s'] = 'Şifre: %s';
+$lang['UAM_PasswordTest'] = 'Şifre test:';
+$lang['UAM_No_Casse'] = 'Kullanıcı Adları: Büyük Küçük harf duyarlı';
+$lang['UAM_NewFeature'] = 'Yeni Özellik!';
+$lang['UAM_Restore_File'] = 'Ayarları Geri Yükle';
+$lang['UAM_PwdReset'] = 'Şifre yenileme';
+$lang['UAM_PwdReset_Done'] = 'Şifre yenilendi';
+$lang['UAM_PwdReset_NA'] = 'Orjinal şifre';
+$lang['UAM_PwdReset_Todo'] = 'Şifre yenilemesi';
+$lang['UAM_Registration_Date'] = 'Kayıt tarihi';
+$lang['UAM_Reminder_Sent_NOK'] = 'HAYIR';
+$lang['UAM_Reminder_Sent_OK'] = 'EVET';
+$lang['UAM_Tab_Global'] = 'Yapılandırma';
+$lang['UAM_SubTitle1'] = 'Plugin yapılandırma';
+$lang['UAM_ScoreTest'] = 'Sonuç: ';
+$lang['UAM_Save'] = 'Backup çalıştır';
+$lang['UAM_User: %s'] = 'Kullanıcı : %s';
+$lang['UAM_Update of %s'] = 'Profil %s güncellendi';
+$lang['UAM_no_update_pwd'] = '[Profil şifre değiştirilmeden güncellendi]';
+$lang['UAM_reg_err_login3'] = 'Güvenlik: Şifre zorunludur.';
+$lang['UAM_empty_pwd'] = '[boş şifre]';
+$lang['UAM_confirmmail_custom_Txt1'] = 'Onay sayfası metni - Onay kabul edildi';
+$lang['UAM_confirmmail_custom_Txt2'] = 'Onay sayfası metni - Onay reddedildi';
+$lang['UAM_Default_ConfirmMail_Txt'] = 'Merhaba [username]!  [mygallery] me üye olduğunuz için teşekkür ederim.
+İşleminizi tamamlamak ve fotoğrafları izleyebilmek için mesajdaki linkle tıklayarak kaydınızı tamamlayabilirsiniz.';
+$lang['UAM_%d_Reminder_Sent'] = '%d hatırlatma mesajı gönderildi';
+$lang['UAM_%d_Reminders_Sent'] = '%d hatırlatma mesajları gönderildi';
+$lang['UAM %d users pwdreseted'] = '%d kullanıcı için şifre yenileme gerekli';
+$lang['UAM %d user pwdreseted'] = '%d kullanıcısı için şifre yenileme gerekli';
+$lang['UAM_Username_Char_true'] = 'Uygunsuz karakter: <br>(Karakterleri ayırmak için virgül kullanın)<br><br>';
+$lang['UAM_Username_Char_false'] = 'Hepsine izin ver (varsayılan)';
+$lang['UAM_Title5'] = 'Çeşitli ayarlar';
+$lang['UAM_Title2'] = 'Üyelik Onaylama';
+$lang['UAM_Title3'] = 'Kayıtsız ziyaretçi yönetimi';
+$lang['UAM_Title4'] = 'İpuçları ve kullanıma ilişkin örnekler';
+$lang['UAM_RedirToProfile'] = 'Özelleştirme sayfasına yönlendir';
+$lang['UAM_Mail_Info'] = 'Kullanıcıya bilgi maili: ';
+$lang['UAM_No_Backup_File'] = 'Backup dosyası bulunamadı!';
+$lang['UAM_Nb_Days'] = 'Günlük fark';
+$lang['UAM_MiscOptions'] = 'Diğer Seçenekler';
+$lang['UAM_Manual_validation_needed_for %s'] = '%s adlı kullanıcı üye oldu ve Admin tarafından manuel onaylama bekliyor';
+$lang['UAM_Manual_Validation'] = 'Manuel onay';
+$lang['UAM_Link: %s'] = 'Üyeliğinizi onaylamak için lütfen linke tıklayın: %s';
+$lang['UAM_LastVisit_Date'] = 'Son Ziyaret';
+$lang['UAM_GTAutoMail'] = 'Grup/durum/güvenlik seviyesi değiştirildiğinde otomatik mail gönderme';
+$lang['UAM_GTAutoGp'] = 'Grup/durum/gizlilik seviyesini otomatik olarak değiştirme';
+$lang['UAM_GTAutoDel'] = 'Hesapların otomatik silinmesi';
+$lang['UAM_Default_ValidationTimeout_Txt'] = 'Üzgünüz, Size tanınan ([Kdays] gün) süre içinde hesabınızı onaylamadığınız için hesabınız silindi. Lütfen tekrar üyelik başvurusunda bulunurken geçerli ve bloke edilmemiş bir email adresi kullanın.';
+$lang['UAM_Default_RejectConnexion_Txt'] = 'Üzgünüz, üyeliğiniz henüz doğrulanmadığı için galeriye bağlanılamıyor.';
+$lang['UAM_Default_InfoMail_Txt'] = 'Merhaba [username]!
+
+[mygallery] Galerideki profilinizde önemli değişiklikler yapılmıştır. Yeni ayarlarınızı aşağıda bulabilirsiniz:
+';
+$lang['UAM_Dump_OK'] = 'YEdekleme dosyası başarıyla oluşturuldu';
+$lang['UAM_Dump_NOK'] = 'Hata: Yedekleme dosyası oluşturulamadı!';
+$lang['UAM_Dump_Download'] = 'Yedeğinizi download etmek için, Lütfen bu kutuyu işaretleyin:';
+$lang['UAM_DumpTxt'] = 'Ayarlarınızı yedekleyin ve tekrar yükleyin';
+$lang['UAM_Delete_selected'] = 'Sil';
+$lang['UAM_Webmaster cannot be pwdreset'] = 'Şifre yenileme webmaster hesabı için ayarlanamaz.';
+$lang['UAM_You cannot pwdreset your account'] = 'Şifre yenileme kendi hesabınız için ayarlanamaz.';
+$lang['UAM_You need to confirm pwdreset'] = 'Şifre yenilmeyi onaylamanız gerekir(onay kutusu)!';
+$lang['UAM_reg_err_login4_%s'] = 'Güvenlik: Kontorl sistemi seçilen şifreler için karmaşık bir kontorl mekanizması sağlar. Şifreniz çok basit (karakter sayısı=%s) Lütfen aşağıdaki kurallara dikkat ederek yeni bir şifre belirleyin: <br>
+- Harf ve rakam içermeli<br>
+- Büyük ve küçük harf içermeli<br>
+- Uzunluğu arttırın (karakter uzunluğunu)<br>
+Yönetici tarafından belirlenmiş minimum şifre uzunluğu:';
+$lang['UAM_reg_err_login5'] = 'E-posta sağlayıcınız kayıt için yasaklanmıştır. Yasaklı eposta sağlayıcıları: ';
+$lang['UAM_reg_err_login2'] = 'Kullanıcı adı izin verilen karakterlerin dışında karakter içeriyor:';
+$lang['UAM_infos_mail %s'] = '%s, burada galeriye giriş bilgilerinizi bulabilirsiniz:';
+$lang['UAM_audit_ok'] = 'Denetim OK';
+$lang['UAM_audit'] = 'Denetim ayarları';
+$lang['UAM_USRAutoDel'] = 'Silinen hesaplar için mesaj ayarlama';
+$lang['UAM_Title1'] = 'Kayıtlar için kısıtlamalar';
+$lang['UAM_Reminder'] = 'Email hatırlatma';
+$lang['UAM_Password_Reset_Msg'] = 'Lütfen Şifrenizi değiştirin!';
+$lang['UAM_No_Ghosts'] = 'Ziyaretçi yok';
+$lang['UAM_Mail_with_key'] = 'Şifre ile hatırlatma';
+$lang['UAM_MailInfo_Text'] = 'Bilgilendirme mail i ayarlama';
+$lang['UAM_Mail_without_key'] = 'Şifresiz hatırlatma';
+$lang['UAM_MailInfo_Subject'] = 'Bilgilendirme email i ayarlama';
+$lang['UAM_Ghost_reminder_of_%s'] = '%s, bu bir hatırlatma e-mail idir.';
+$lang['UAM_GTAuto'] = 'Ziyaretçiler için otomatik yönetim';
+$lang['UAM_Default_GhstDeletion_Txt'] = '[username] Üzgünüz, uzun süredir [mygallery] e girmediğiniz için hesabınız silindi. Tekrar giriş yapabilmek için lütfen üye olun.';
+$lang['UAM_Default_GTReminder_Subject'] = '[username]- [mygallery] i son ziyaretinizden bu yana uzun zaman geçti';
+$lang['UAM_Default user cannot be pwgreset'] = 'Şifre yenileme varsayılan kullanıcı hesabı için ayarlanamaz!';
+$lang['UAM_Default_CfmMail_Custom_Txt2'] = 'Yanlış / Süresi geçmiş doğrulama kodu, hesap zaten başarıyla doğrulanmış';
+$lang['UAM_Default_CfmMail_Custom_Txt1'] = '[username] hesabı başarıyla doğrulandı!';
+$lang['UAM_Default_AdminValidationMail_Subject'] = '[username] - [mygallery] e Kayıt başarıyla doğrulandı.';
+$lang['UAM_Default_AdminValidation_Txt'] = 'Merhaba [username]! [mygallery] Galerimize kayıt olduğunuz için teşekkür ederiz.
+
+[mygallery] üyeliğiniz admin tarafından onaylanmıştır. Profil bilgilerinizde değişiklik yapabilmek için  [myurl] ye giriş yapabilirsiniz.';
+$lang['UAM_Customize_messagesandmails'] = 'Mesaj ve email özelleştirm';
+$lang['UAM_CustomPasswRetr'] = 'Şifre hatırlatma email taslağı hazırlama';
+$lang['UAM_Confirm_Mail_true'] = 'Kullanıcı onayı etkinleştirme';
+$lang['UAM_Confirm_Mail_local'] = 'Admin onayı etkinleştirme';
+$lang['UAM_Confirm_Mail'] = 'Kayıt Onayı:';
+$lang['UAM_ConfirmMail_Remail'] = 'Onaylanmamış kulllanıcıları hatırlat';
+$lang['UAM_ConfirmMail_Subject'] = 'Kayıt Email i oluşturma';
+$lang['UAM_ConfirmMail_Text'] = 'Onaylama E-mail i oluşturma:';
+$lang['UAM_AdminConfMail'] = 'Adminler için kayıt onay';
+$lang['UAM_Add of %s'] = 'Profil %s için oluşturuldu';
+$lang['UAM_%d_Validated_Users'] = '% Kullanıcılar onaylandı';
+$lang['UAM_%d_Validated_User'] = '% Kullanıcı onaylandı';
+$lang['UAM_Disable'] = 'Devre Dışı [varsayılan]';
+$lang['UAM_Enable'] = 'Etkinleştir';
+$lang['UAM_Default_ConfirmMail_Subject'] = '[username] adı [mygallery] kayıt onayı aşamasında';
+$lang['UAM_Default_ConfirmMail_Remail_Subject'] = 'Hatırlatma- [username] adı [mygallery] kayıt onayı aşamasında';
+$lang['UAM_Demotion of %s'] = '%s İndirgeme';
+$lang['UAM_AdminValidationMail_Subject'] = 'Elle doğrulama e-posta konusunu özelleştir';
+$lang['UAM_AdminPassword_Enforced'] = 'Yöneticilere uygulanıyor';
+$lang['UAM_%d_Mails_With_Key'] = 'yenileme anahtarı içeren %d mesaj gönderildi';
+$lang['UAM_%d_Mail_With_Key'] = 'yenileme anahtarı içeren %d mesaj gönderildi';
+$lang['UAM_Admins cannot be pwdreset'] = 'Yönetici hesapları için şifre yenilemesi ayarlanamaz!';
+$lang['UAM_Err_audit_username_char'] = '<b>Bu hesap bir veya daha fazla yasaklı karakter kullanıyor:</b>';
+$lang['UAM_Err_audit_email_forbidden'] = '<b>Bu hesap yasaklı bir e-posta sağlayıcısı kullanıyor:</b>';
+$lang['UAM_GT_Init'] = 'Hayalet Takipçisi Başlatılıyor';
+$lang['UAM_GT_Reset'] = 'Hayalet Takipçisini Sıfırla';
+$lang['UAM_GhostTracker_true'] = 'Etkinleştir. İki ziyaret arasında azami süre: ';
+$lang['UAM_Guest cannot be pwdreset'] = 'Misafir hesabı için şifre yenileme ayarlanamaz!';
+$lang['UAM_HidePassw'] = 'Bilgi e-postasında açık yazı şeklinde şifre';
+$lang['UAM_GhostTracker_ReminderText'] = 'Hayalet Takipçisi hatırlatma mesajının özelleştirilmesi';
+$lang['UAM_GhostTracker_Init_OK'] = 'Hayalet Takipçisi sıfırlandı!';
+$lang['UAM_Generic cannot be pwdreset'] = 'Jenerik hesaplar için şifre yenileme ayarlanamaz!';
+$lang['UAM_GhostTracker'] = 'Hayalet ziyaretçilerin yönetimi (Hayalet Takipçisi)';
+$lang['UAM_ConfirmMail_TimeOut_true'] = 'Etkin. Kullanım süresinin dolmasına bu kadar gün kaldı:';
+$lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Doğrulama anahtarının yenilenmesi <b><u>olmadan</u></b> hatırlatma mesajı özelleştirme.';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Doğrulama anahtarının yenilenmesi <b><u>ile</u></b> hatırlatma mesajı özelleştirme.';
+$lang['UAM_ConfirmMail_ReMail_Subject'] = 'Kayıt doğrulama hatırlatma e-posta konusunu özelleştir';
+$lang['UAM_Bad_version_backup'] = 'Yedek dosyasınınki ile eklenti versiyonu uyuşmuyor! Onarım yapılmadı.';
+$lang['UAM_AdminValidationMail_Text'] = 'Elle doğrulama işlemi bilgilendirme mesajı';
+$lang['UAM_No_Userlist'] = 'Şu an için listelenecek ziyaretçi bulunmuyor';
+$lang['UAM_GhostTracker_Title'] = 'Hayalet ziyaretçilerin yönetimi';
+$lang['UAM_Change'] = 'Doğrulanan kullanıcıların Grup/Durum/Gizlilik seviyelerini değiştirir';
+$lang['UAM_Confirm_Group'] = 'Gruplar<br>(grubu etkilememesi için ------- olarak bırakın)';
+$lang['UAM_Confirm_Status'] = 'Durumlar<br>(Piwigo varsayılanı için ------- olarak bırakın)';
+$lang['UAM_Confirm_Level'] = 'Gizlilik seviyesi<br>(Piwigo varsayılanı için ------- olarak bırakın)';
+$lang['UAM_Expired_Group'] = 'Kullanıcının kayıtlı olduğu <b>grubun</b> geçerliliği sona ermiş durumdadır.<br>';
+$lang['UAM_Confirm_grpstat_notice'] = 'Dikkat: Grup yada doğrulama durumlarından sadece birini kullanmanız önerilir. Gizlilik seviyesi bir grup ya da durum atama ile bağlantılı olarak kullanılabilir.';
+$lang['UAM_Reminder_without_key_of_%s'] = '%s, Onay kodunuzun süresi doldu';
+$lang['UAM_ValidationLimit_Info'] = 'Kayıt onay sınırı için son tarih';
+$lang['UAM_USRAutoMail'] = 'Otomatik E-Posta hatırlatma';
+$lang['UAM_USRAuto'] = 'Doğrulanmamış kullanıcıların otomatik yönetimi';
+$lang['UAM_Tracking registered users'] = 'Kayıtlı kullanıcı takibi';
+$lang['UAM_Tracking confirmations'] = 'Takip onayları';
+$lang['UAM_Title_Tab'] = 'UsrAdvManager - Versiyon:';
+$lang['UAM_Restoration_OK'] = 'apılandırma geri yüklemeniz başarıyla tamamlandı. Restore ayarlarını görüntülemek için UAM yönetim sayfasını yeniden yükleyin.';
+$lang['UAM_Restoration_NOK'] = 'Yapılandırma geri yükleme sırasında bir hata oluştu. Yedekleme dosya geçerliliğini kontrol ediniz.';
+$lang['UAM_Stuffs_Desc'] = 'Doğrulanmamış kullanıcılar için bir bilgi bloğu ekler';
+$lang['UAM_Reminder_with_key_of_%s'] = '%s, onay key i  yenilendi.';
+$lang['UAM_RejectConnexion_Custom_Txt'] = 'Reddetme metni düzenleme';
+$lang['UAM_RejectConnexion'] = 'Kaydı onaylayana kadar kullanıcının bağlantısını redddet';
+$lang['UAM_Password_Enforced'] = 'Şifre güvenlik düzeyinin güçlendirilmesi';
+$lang['UAM_Password reset selected users'] = 'Seçilen kullanıcılar için şifre yenileme isteği';
+$lang['UAM_GTAutomail_Text'] = 'İndirgeme e-posta metnini düzenleyin';
+$lang['UAM_Default_PwdRequest_Txt'] = 'Galerimize şifre yenileme isteğinde bulundunuz. Yeni bağlantı ayarlarınızı aşağıdaki linkten yapabilirsiniz.';
+$lang['UAM_Default_InfoMail_Subject'] = '[username] - [mygalley] Galerisi Kayıt bilgileriniz ';
+$lang['UAM_Default_GhstReminder_Txt'] = 'Merhaba [username].
+
+Bu bilgilendirme mesajının gönderilmesini  sebebi [mygallery] galerimize son girişiniden bugüne uzun süre geçmiş olmasıdır. Eğer üyeliğinizi kullanmak istemiyorsanız bu mail i cevaplayarak bunu bize iletirseniz üyeliğinizi silebiliriz.
+
+Bu mesajın alındığı günden itibaren [days] gün de galeriye herhangi bir giriş yapmazsanız üyeliğiniz otoamatik olarak silinecektir.
+
+Saygılarımızla,
+
+[mygallery] Galeri yöneticisi - [myurl]';
+$lang['UAM_Default_GTAutoMail_Subject'] = '[username] - Hesabınız [mygallery] de indirgendi.';
+$lang['UAM_Default_CfmMail_Remail_Txt2'] = 'Merhaba [username].
+
+Bu hatırlatma mesajının gönderilme amacı üye olduüunuz [mygallery] galerimizdeki üyeliğinizi aktif etmediniz. Galerimizi görebilmeniz için üyeliğinizi aktif etmeniz gerekmektedir. x gün içinde üyeliğinizi aktif edebilmeniz için üye olduğunuzda size gönderilen mesajdaki linke tıklayarak aktif etmeniz gerekiyor.
+
+Not: Eğer bu süre içinde üyeliğinizi aktif etmezseniz üyeliğiniz silinecektir.';
+$lang['UAM_Default_CfmMail_Remail_Txt1'] = 'Merhaba [username].
+
+[mygallery] galerimize üye olduğunuz için bu bir hatırlatma mesajıdır. Galerimize üye oldunuz fakat üyeliğğinizi aktif etmediğiniz için üelik onay kodunuzun süresi dolmuştur. Üyeliğinizi aktif etmeniz için onay süreciniz yenilenmiştir. x gün içinde üyeliğinizi aktif edebilirsiniz.
+
+Not: Eğer bu süre içinde üyeliğinizi aktif etmezseniz üyeliğiniz silinecektir.';
+$lang['UAM_No_validation_for_your_account'] = 'Kişisel yönetici hesabınız doğrulamaya tabi değildir';
+$lang['UAM_No_validation_for_default_user'] = 'Varsayılan hesap doğrulamaya tabi değildir';
+$lang['UAM_No_validation_for_Webmaster'] = '"Webmaster" hesabı doğrulamaya tabi değildir';
+$lang['UAM_group "%s" updated'] = '%s grubu en iyi performans için otomatik olarak varsayılan grup olarak ayarlandı.';
+$lang['UAM_Validation of %s'] = '%s doğrulama';
+$lang['UAM_MailExclusion_No'] = 'e-Posta alan adlarını (domain) dışlama - Kayıtlar için e-Posta mecburiyeti olmadığından kullanılamaz';
+$lang['UAM_MailExclusion'] = 'e-Posta alan adlarını (domain) dışlama';
+$lang['UAM_No_Usermanager'] = 'Şu anda doğrulanmamış kayıt bulunmamaktadır';
+$lang['UAM_No_Confirm_Status'] = 'Kayıtlarını henüz doğrulamayan üye durumları<br>';
+$lang['UAM_Expired_Level'] = '<b>Gizlilik seviyesi</b> kullanıcı kayıt süresi doldu<br>';
+$lang['UAM_Expired_Status'] = '<b>Durum</b> kullanıcının kayıt süresi doldu<br>';
+?>
