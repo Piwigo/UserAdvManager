6b1e5288a68cf378e2054e9ad609db28582bbe88
Eric <eric-piwigo@users.noreply.github.com>
2013-05-25 13:00:39 +0000

    r22868 merged from trunk to branch 2.50 : Add ru_RU, thanks to : Konve Add pt_BR, thanks to : hespanhol Update tr_TR, thanks to : hakanselcuk

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,177 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_Default_PwdRequest_Txt'] = 'Вы запросили смену пароля для доступа в нашу галерею. Пожалуйста, ознакомьтесь с Вашими новыми настройками для соединения.';
+$lang['UAM_Default_InfoMail_Txt'] = 'Здравствуйте, [username]!
+Вы внесли значительные изменения в Ваш профиль в галерее [mygallery]. Пожалуйста, ознакомьтесь с новыми настройками:';
+$lang['UAM_Default_RejectConnexion_Txt'] = 'Извините, Ваша регистрация еще не завершена, поэтому Вы не можете сейчас войти в галерею.';
+$lang['UAM_Default_ValidationTimeout_Txt'] = 'Извините, Ваш аккаунт был удален, потому что Вы не подтвердили свою регистрацию в требуемое время ([Kdays] дней). Пожалуйста, повторите регистрацию с работающим и незаблокированным электронным адресом.';
+$lang['UAM_Demotion of %s'] = 'Понижение пользователя %s';
+$lang['UAM_Delete_selected'] = 'Удаление';
+$lang['UAM_DumpTxt'] = 'Резервное копирование и восстановление Вашей конфигурации';
+$lang['UAM_Dump_Download'] = 'Для загрузки резервного файла, пожалуйста, поставьте отметку в окне:';
+$lang['UAM_Dump_NOK'] = 'Ошибка: невозможно создать резервный файл!';
+$lang['UAM_Dump_OK'] = 'Резервный файл создан успешно';
+$lang['UAM_Enable'] = 'Включено';
+$lang['UAM_Disable'] = 'Выключено (по умолчанию)';
+$lang['UAM_Default_InfoMail_Subject'] = '[username] - Ваша регистрационная информация в [mygallery]';
+$lang['UAM_Default_GhstReminder_Txt'] = 'Здравствуйте, [username].
+	
+Это письмо-напоминание о том, что прошло очень много времени с Вашего последнего посещения галереи [mygallery]. Если Вы больше не хотите иметь этот аккаунт, пожалуйста, сообщите нам об этом, ответив на этот e-mail. Ваш аккаунт будет удален.
+
+По получении этого сообщения и отсутствии Ваших новых визитов в течение [days] дней, мы автоматически удалим Ваш аккаунт.
+
+С уважением,
+
+Администратор галереии [mygallery] - [myurl]';
+$lang['UAM_Default_GhstDemotion_Txt'] = 'Извините, [username], Ваш аккаунт устарел из-за слишком большого времени, прошедшего с Вашего последнего визита в галерею [mygallery]. Пожалуйста, перейдите по ссылке и подтвердите свой аккаунт:';
+$lang['UAM_Default_GhstDeletion_Txt'] = 'Извините, [username], Ваш аккаунт был удален из-за слишком большого времени, прошедшего с Вашего последнего визита в галерею [mygallery]. Пожалуйста, зарегистрируйтесь снова.';
+$lang['UAM_LastVisit_Date'] = 'Последний визит';
+$lang['UAM_Ghost_reminder_of_%s'] = '%s, это письмо с напоминанием';
+$lang['UAM_reg_err_login5'] = 'С Вашим провайдером эл.почты регистрация недоступна. ';
+$lang['UAM_reg_err_login2'] = 'Имя пользователя не должно содержать следующие символы:';
+$lang['UAM_submit'] = 'Отправка настроек';
+$lang['UAM_save_config'] = 'Конфигурация сохранена.';
+$lang['UAM_reg_err_login3'] = 'Служба безопасности: Пароль обязателен!';
+$lang['UAM_Subject admin validation for %s'] = 'Регистрация пользователя %s ожидает подтверждения';
+$lang['UAM_Title4'] = 'Советы и примеры использования';
+$lang['UAM_Title5'] = 'Прочие функции';
+$lang['UAM_Title2'] = 'Подтверждение регистрации';
+$lang['UAM_Support_txt'] = 'Официальная поддрежка этого плагина только на этом форуме Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=15015" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=15015</a><br><br>
+Еще можете обратиться на бэгтрэкер проекта: <a href="http://piwigo.org/bugs/" onclick="window.open(this.href);return false;">http://piwigo.org/bugs/</a>';
+$lang['UAM_SubTitle1'] = 'Конфигурация плагина';
+$lang['UAM_Stuffs_Title'] = 'Блок плагина UAM';
+$lang['UAM_Stuffs'] = 'Блок плагина PWG Stuff';
+$lang['UAM_ScoreTest'] = 'Результат:';
+$lang['UAM_No_validation_for_Guest'] = 'Аккаунт гостя не нуждается в подтверждении';
+$lang['UAM_No_validation_for_Webmaster'] = 'Аккаунт вебмастера не нуждается в подтверждении';
+$lang['UAM_Manual_Validation'] = 'Подтверждение вручную';
+$lang['UAM_Manual_validation_needed_for %s'] = 'Пользователь %s зарегистрирован и ожидает "ручного" подтверждения от администратора.';
+$lang['UAM_MiscOptions'] = 'Другие дополнительные опции';
+$lang['UAM_Default_AdminValidation_Txt'] = 'Здравствуйте, [username]! Спасибо за регистрацию и добро пожаловать в галерею [mygallery].
+
+Ваш аккаунт был подтвержден вручную вебмастером галереи [mygallery]. Вы можете войти под своим логином [myurl] и сделать необходимые изменения и дополнения в своем профиле.';
+$lang['UAM_Default_CfmMail_Custom_Txt1'] = '[username] аккаунт успешно подтвержден!';
+$lang['UAM_Default_CfmMail_Custom_Txt2'] = 'Некорректный/устаревший ключ активации или аккаунт уже подтвержден.';
+$lang['UAM_Default_CfmMail_Remail_Txt1'] = 'Здравствуйте, [username].
+
+Это письмо-напоминание о том, что Вы зарегистрировались в нашей галерее [mygallery], но не подтвердили свою регистрацию, и Ваш ключ подтверждения устарел. Чтобы всё-таки обеспечить Ваш доступ к нашей галерее, мы увеличили период подтверждения. У Вас есть еще x дней для подтверждения Вашей регистрации.
+
+Учтите: после указанного периода Ваш акааунт будет безвозвратно удален.';
+$lang['UAM_Default_CfmMail_Remail_Txt2'] = 'Здравствуйте, [username].
+
+Это письмо-напоминание о том, что Вы зарегистрировались в нашей галерее [mygallery], но не подтвердили свою регистрацию, а Ваш ключ подтверждения скоро устареет. Для обеспечения доступа к нашей галерее у Вас есть x дней. Подтвердите свою регистрацию, перейдя по ссылке, которую Вы получили после Вашей регистрации.
+
+Учтите: после указанного периода Ваш акааунт будет безвозвратно удален.';
+$lang['UAM_Link: %s'] = 'Пожалуйста, перейдите по ссылке для подтверждения регистрации: %s';
+$lang['UAM_NewFeature'] = 'Новая фишка!';
+$lang['UAM_No_Backup_File'] = 'Файл резервного восстановления недоступен!';
+$lang['UAM_No_Confirm_Group'] = 'Группа пользователей, которые не подтвердили свою регистрацию<br>';
+$lang['UAM_No_Confirm_Status'] = 'Статус пользователей, которые не подтвердили свою регистрацию<br>';
+$lang['UAM_Reminder'] = 'Эл.письмо с напоминанием';
+$lang['UAM_Save'] = 'Запуск резервного копирования';
+$lang['UAM_Tab_Global'] = 'Конфигурация';
+$lang['UAM_Tips1'] = 'Информация о неподтвержденной регистрации с UAM и PWG_Stuffs';
+$lang['UAM_Tips2'] = 'Информация о неподтвержденной регистрации с UAM и Additional Pages';
+$lang['UAM_Title_Tab'] = 'Версия UserAdvManager: ';
+$lang['UAM_Reminder_Sent_NOK'] = 'НЕТ';
+$lang['UAM_Reminder_Sent_OK'] = 'ДА';
+$lang['UAM_Reminder_with_key_of_%s'] = '%s, Ваш ключ подтверждения был обновлен';
+$lang['UAM_Reminder_without_key_of_%s'] = '%s, Ваш ключ подтверждения устарел';
+$lang['UAM %d user pwdreseted'] = 'Требуется обновить пароль для %d пользователя';
+$lang['UAM %d users pwdreseted'] = 'Требуется обновить пароли для %d пользователей';
+$lang['UAM_%d_Mail_With_Key'] = '%d сообщение с ключом обновления было отправлено';
+$lang['UAM_%d_Mails_With_Key'] = '%d сообщения с ключом обновления были отправлены';
+$lang['UAM_%d_Reminder_Sent'] = '%d письмо с напоминанием было отправлено';
+$lang['UAM_%d_Reminders_Sent'] = '%d письма с напоминаниями были отправлены';
+$lang['UAM_%d_Validated_User'] = '%d Пользователь подтвержден вручную';
+$lang['UAM_%d_Validated_Users'] = '%d Пользователи подтверждены вручную';
+$lang['UAM_Add of %s'] = 'Профиль создан для %s';
+$lang['UAM_AdminConfMail'] = 'Подтверждение регистрации для администраторов';
+$lang['UAM_GTAuto'] = 'Автоматическое управление пользователями-призраками';
+$lang['UAM_GTAutoDel'] = 'Автоматическое удаление аккаунтов';
+$lang['UAM_GTAutoGp'] = 'Автоматическая смена группы/статуса/личного уровня';
+$lang['UAM_GTAutoMail'] = 'Автоматическая отправка эл.письма при смене группы/статуса/личного уровня';
+$lang['UAM_Err_UserManager_Settings'] = 'Эта страница доступна, только если активирована функция "Подтверждение регистрации", и если группа посетителей не подтверждена в разделе "Установки подтверждения регистрации".';
+$lang['UAM_Err_Userlist_Settings'] = 'Эта страница доступна, только если активирована функция "Мониторинг зарегистрированных пользователей" в разделе "Установки регистрации и другие функции". ';
+$lang['UAM_Err_audit_email_forbidden'] = '<b>Этот аккаунт использует запрещенную эл.почту:</b> ';
+$lang['UAM_Err_audit_username_char'] = '<b>Этот аккаунт использует один или больше запрещенных символов:</b> ';
+$lang['UAM_Restore_File'] = 'Восстановление конфигурации';
+$lang['UAM_Registration_Date'] = 'Регистрационные данные';
+$lang['UAM_Password reset selected users'] = 'Запрос обновления пароля для выбранных пользователей';
+$lang['UAM_Password: %s'] = 'Пароль: %s';
+$lang['UAM_PasswordTest'] = 'Проверка пароля:';
+$lang['UAM_Password_Reset_Msg'] = 'Пожалуйста, смените свой пароль!';
+$lang['UAM_PwdReset'] = 'Обновление пароля';
+$lang['UAM_PwdReset_Done'] = 'Пароль обновлен';
+$lang['UAM_PwdReset_NA'] = 'Исходный пароль';
+$lang['UAM_You need to confirm pwdreset'] = 'Вы должны подтвердить обновление пароля (поставьте галочку)!';
+$lang['UAM_group "%s" updated'] = 'Для лучшей производительности группа %s была автоматически установлена в качестве "группы по умолчанию".';
+$lang['UAM_no_update_pwd'] = '[профиль обновлен без смены пароля]';
+$lang['UAM_Update of %s'] = 'Профиль %s обновлен';
+$lang['UAM_User: %s'] = 'Пользователь: %s';
+$lang['UAM_audit'] = 'Установки аудита';
+$lang['UAM_audit_ok'] = 'Результат аудита - ОК';
+$lang['UAM_Default_ConfirmMail_Subject'] = 'Регистрация пользователя [username] в галерее [mygallery] подтверждена';
+$lang['UAM_ConfirmMail_Remail'] = 'Напоминание неподтвержденным пользователям';
+$lang['UAM_ConfirmMail_TimeOut_true'] = 'Включено. Количество дней до истечения срока действия:';
+$lang['UAM_Confirm_Mail'] = 'Подтверждение регистрации:';
+$lang['UAM_Confirm_Mail_local'] = 'Включено - Подтверждено администратором';
+$lang['UAM_Confirm_Mail_true'] = 'Включено - Подтверждено пользователем';
+$lang['UAM_AdminPassword_Enforced'] = 'Применение к администраторам ';
+$lang['UAM_Admins cannot be pwdreset'] = 'Возобновление пароля не может быть установлено для аккаунта администратора!';
+$lang['UAM_Bad_version_backup'] = 'Версия файла резервной копии не соответствует версии плагина! Восстановление не была выполнено.';
+$lang['UAM_Username_Char_false'] = 'Разрешить всё (по умолчанию)';
+$lang['UAM_Username_Char_true'] = ' Запрещенные символы:<br>(Используйте запятую, чтобы отделить каждый символ)<br><br>';
+$lang['UAM_Valid_Level'] = 'Персональный уровень пользователей, которые подтвердили свою регистрацию <br>';
+$lang['UAM_Validated_Group'] = 'Группа пользователей, которые подтвердили свою регистрацию <br>';
+$lang['UAM_Validated_Status'] = 'Статус для пользователей, которые подтвердили свою регистрацию <br>';
+$lang['UAM_Webmaster cannot be pwdreset'] = 'Возобновление пароля нельзя установить для аккаунта вебмастера!';
+$lang['UAM_You cannot pwdreset your account'] = 'Возобновление пароля нельзя установить для своего собственного аккаунта!';
+$lang['UAM_confirmmail_custom_Txt1'] = 'Текст на странице подтверждения - Подтверждение прошло успешно';
+$lang['UAM_confirmmail_custom_Txt2'] = 'Текст на странице подтверждения - Подтверждение не удалось';
+$lang['UAM_empty_pwd'] = '[пустой пароль]';
+$lang['UAM_infos_mail %s'] = '%s, пожалуйста, найдите здесь Вашу информацию для входа в галерею:';
+$lang['UAM_reg_err_login4_%s'] = 'Бесплатный совет от службы безопасности:<br> Система контроля, установленная на сайте, вычисляет уровень сложности задаваемых паролей. Сложность Вашего текущего пароля весьма низкая (уровень  = %s). Пожалуйста, придумайте и введите новый пароль, более серьёзный. Для этого используйте следующие правила: <br>
+- Используйте в пароле и буквы и цифры <br>
+- Используйте для вводимых символов и верхний и нижний регистры <br>
+- Используйте увеличенную длину пароля (чем больше символов, тем надежнее) <br>
+Минимальный уровень сложности пароля, установленный администрацией сайта:';
+$lang['UAM_PwdReset_Todo'] = 'Пароль был возобновлен';
+$lang['UAM_RedirToProfile'] = 'Перенаправление на страницу настроек';
+$lang['UAM_RejectConnexion'] = 'Пользователю отказано в соединении до тех пор, пока регистрация не будет подтверждена';
+$lang['UAM_RejectConnexion_Custom_Txt'] = 'Настроить текст с отказом пользователю';
+$lang['UAM_Restoration_OK'] = 'Восстановление Вашей конфигурации завершено успешно! Пожалуйста, перезагрузите страницу администрирования плагина UAM, чтобы увидеть восстановленные настройки.';
+$lang['UAM_Stuffs_Desc'] = 'Добавление информационного блока для неподтвержденных пользователей';
+$lang['UAM_Title1'] = 'Ограничения регистраций';
+$lang['UAM_Tracking confirmations'] = 'Отслеживание подтверждений';
+$lang['UAM_Tracking registered users'] = 'Отследживание зарегистрированных пользователей';
+$lang['UAM_USRAuto'] = 'Автоматическое управление неподтвержденными пользователями';
+$lang['UAM_USRAutoDel'] = 'Настройка сообщения удаленным аккаунтам';
+$lang['UAM_USRAutoMail'] = 'Автоматизированное напоминание по e-mail';
+$lang['UAM_Username_Char'] = 'Имена пользователей: недопустимые символы';
+$lang['UAM_Default_AdminValidationMail_Subject'] = '[username] - Регистрация на сайте [mygallery] подтверждена';
+$lang['UAM_Default_GTAutoMail_Subject'] = '[username] - Ваш аккаунт на сайте [mygallery] был понижен';
+$lang['UAM_Default_GTReminder_Subject'] = '[username] - Слишком много времени пришло с Вашего последнего визита на сайт [mygallery]';
+$lang['UAM_AdminValidationMail_Text'] = 'Уведомление о ручном подтверждении регистрации';
+?>
