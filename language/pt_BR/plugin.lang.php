8303d427110ad8889b54dd70566f0b8928f36efb
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2013-05-31 15:14:40 +0000

    [useradvmanager] Update pt_BR, thanks to : flaviove

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -60,4 +60,50 @@ $lang['UAM_Confirm_Mail_local'] = 'Habilitar - Confirmação pelo Administrador'
 $lang['UAM_Confirm_Mail_true'] = 'Habilitar - Confirmação pelo Usuário';
 $lang['UAM_CustomPasswRetr'] = 'Configurar o conteúdo do email de senha perdida';
 $lang['UAM_Customize_messagesandmails'] = 'Configuração de mensagens e emails';
+$lang['UAM_Title1'] = 'Restrições para inscrições';
+$lang['UAM_Title2'] = 'Confirmações de inscrição';
+$lang['UAM_Title3'] = 'Gestão de usuários fantasma';
+$lang['UAM_Title4'] = 'Dicas e exemplos de uso';
+$lang['UAM_Title5'] = 'Opções diversas';
+$lang['UAM_Title_Tab'] = 'UserAdvManager - versão:';
+$lang['UAM_Tracking confirmations'] = 'Confirmações de rastreamento';
+$lang['UAM_Tracking registered users'] = 'Rastreamento de usuários registrados';
+$lang['UAM_USRAuto'] = 'Gerenciamento automático de usuários não confirmados';
+$lang['UAM_USRAutoDel'] = 'Mensagem personalizada em conta excluída';
+$lang['UAM_USRAutoMail'] = 'Lembrete de e-mail automático';
+$lang['UAM_Update of %s'] = 'Perfil %s atualizado';
+$lang['UAM_User: %s'] = 'Usuário: %s';
+$lang['UAM_Username_Char'] = 'Nomes de usuários: Exclusão de caracteres';
+$lang['UAM_Username_Char_false'] = ' Permita-tudo (default)';
+$lang['UAM_Username_Char_true'] = 'Caracteres proibidos: <br>(Use uma vírgula para separar cada caracter)<br><br>';
+$lang['UAM_Valid_Level'] = 'Nível de privacidade para usuários que tenham confirmado a sua inscrição<br>';
+$lang['UAM_Validated_Group'] = 'Grupo para usuários que tenham confirmado a sua inscrição<br>';
+$lang['UAM_Validated_Status'] = 'Status para usuários que tenham confirmado a sua inscrição<br>';
+$lang['UAM_Validation of %s'] = 'Confirmação de %s';
+$lang['UAM_ValidationLimit_Info'] = 'Prazo para confirmação de inscrição limitado';
+$lang['UAM_Webmaster cannot be pwdreset'] = 'Renovação de senha não pode ser definido para a conta webmaster!';
+$lang['UAM_You cannot pwdreset your account'] = 'Renovação de senha não pode ser definido por sua própria conta!';
+$lang['UAM_You need to confirm pwdreset'] = 'Você deve confirmar renovação de senha (caixa de seleção)!';
+$lang['UAM_audit'] = 'Configurações de Auditoria';
+$lang['UAM_audit_ok'] = 'Auditoria OK';
+$lang['UAM_confirmmail_custom_Txt1'] = 'Texto da página de confirmação - Confirmação de aceite';
+$lang['UAM_confirmmail_custom_Txt2'] = 'Texto da página de confirmação - Confirmação rejeitada';
+$lang['UAM_empty_pwd'] = '[senha em branco]';
+$lang['UAM_group "%s" updated'] = 'Para um melhor desempenho, o grupo %s foi automaticamente definido como o grupo padrão.';
+$lang['UAM_infos_mail %s'] = '%s, por favor encontrar aqui as suas informações de conexão na galeria:';
+$lang['UAM_mail_exclusion_error'] = 'A opção do Piwigo "O endereço de email é obrigatório para todos os usuários", foi desativado enquanto opção plug-in "Exclusão de domínios de correio" do UserAdvancedManager (UAM) manteve-se ativo. Esta combinação é ilógica e não é compatível! <br>
+Por favor, reative a opção do Piwigo "O endereço de e-mail é obrigatório para todos os usuários" para poder, em seguida, desativar a opção "Exclusão de domínios de correio" da UAM se é isso que você quer. <br>
+Nota:. Esta mensagem vai desaparecer depois de ter feito a correção necessária e recarregar a página de administração <br>';
+$lang['UAM_mail_exclusionlist_error'] = 'Atenção! Você entrou em uma nova linha (CR-LF) no início da lista de exclusão de e-mail (mostrado em vermelho abaixo). Embora esta nova linha não seja visível, ele ainda está presente e pode causar o mau funcionamento do plugin. Por favor, re-digite sua lista de exclusão de uma maneira que não começa com uma nova linha.';
+$lang['UAM_no_update_pwd'] = '[Perfil atualizado sem alteração de senha]';
+$lang['UAM_reg_err_login2'] = 'Nome de usuário não tem que coincidir com os seguintes caracteres:';
+$lang['UAM_reg_err_login3'] = 'Segurança: Senha é obrigatório!';
+$lang['UAM_reg_err_login4_%s'] = 'Segurança: Um sistema de controle calcula uma pontuação na complexidade das senhas escolhidas. A complexidade de sua senha é muito baixa (pontuação=%s). Por favor, escolha uma nova senha mais segura, seguindo as seguintes regras: <br>
+- Use letras e números <br>
+- Use maiúsculas e minúsculas<br>
+- Aumentar o comprimento (número de caracteres) <br>
+A pontuação mínima exigido nas senhas pelo administrador é:';
+$lang['UAM_reg_err_login5'] = 'Seu provedor de e-mail está proibido de inscrição. Os Provedores de email proibidos são:';
+$lang['UAM_save_config'] = 'Configurações salvas';
+$lang['UAM_submit'] = 'Submeter configurações';
 ?>
