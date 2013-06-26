ef7951ba8766b5a69b751ef65a9cb61d220d0f80
Eric <eric-piwigo@users.noreply.github.com>
2013-06-26 16:57:25 +0000

    Merged from trunk to branche 2.50 : Update ru_RU, thanks to : Konve and nadusha Update pt_BR, thanks to : flaviove Update tr_TR, thanks to : hakanselcuk Missing translation keys fixed

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -60,4 +60,102 @@ $lang['UAM_Confirm_Mail_local'] = 'Habilitar - Confirmação pelo Administrador'
 $lang['UAM_Confirm_Mail_true'] = 'Habilitar - Confirmação pelo Usuário';
 $lang['UAM_CustomPasswRetr'] = 'Configurar o conteúdo do email de senha perdida';
 $lang['UAM_Customize_messagesandmails'] = 'Configuração de mensagens e emails';
+$lang['UAM_Title1'] = 'Restrições para inscrições';
+$lang['UAM_Title2'] = 'Confirmações de inscrição';
+$lang['UAM_Title3'] = 'Gestão de usuários fantasma';
+$lang['UAM_Title4'] = 'Dicas e exemplos de uso';
+$lang['UAM_Title5'] = 'Opções diversas';
+$lang['UAM_Title_Tab'] = 'UserAdvManager - versão:';
+$lang['UAM_Tracking confirmations'] = 'Confirmações de rastreamento';
+$lang['UAM_Tracking registered users'] = 'Rastreamento de usuários registrados';
+$lang['UAM_USRAuto'] = 'Gerenciamento automático de usuários não confirmados';
+$lang['UAM_USRAutoDel'] = 'Mensagem personalizada em conta excluída';
+$lang['UAM_USRAutoMail'] = 'Lembrete de e-mail automático';
+$lang['UAM_Update of %s'] = 'Perfil %s atualizado';
+$lang['UAM_User: %s'] = 'Usuário: %s';
+$lang['UAM_Username_Char'] = 'Nomes de usuários: Exclusão de caracteres';
+$lang['UAM_Username_Char_false'] = ' Permita-tudo (default)';
+$lang['UAM_Username_Char_true'] = 'Caracteres proibidos: <br>(Use uma vírgula para separar cada caracter)<br><br>';
+$lang['UAM_Valid_Level'] = 'Nível de privacidade para usuários que tenham confirmado a sua inscrição<br>';
+$lang['UAM_Validated_Group'] = 'Grupo para usuários que tenham confirmado a sua inscrição<br>';
+$lang['UAM_Validated_Status'] = 'Status para usuários que tenham confirmado a sua inscrição<br>';
+$lang['UAM_Validation of %s'] = 'Confirmação de %s';
+$lang['UAM_ValidationLimit_Info'] = 'Prazo para confirmação de inscrição limitado';
+$lang['UAM_Webmaster cannot be pwdreset'] = 'Renovação de senha não pode ser definido para a conta webmaster!';
+$lang['UAM_You cannot pwdreset your account'] = 'Renovação de senha não pode ser definido por sua própria conta!';
+$lang['UAM_You need to confirm pwdreset'] = 'Você deve confirmar renovação de senha (caixa de seleção)!';
+$lang['UAM_audit'] = 'Configurações de Auditoria';
+$lang['UAM_audit_ok'] = 'Auditoria OK';
+$lang['UAM_confirmmail_custom_Txt1'] = 'Texto da página de confirmação - Confirmação de aceite';
+$lang['UAM_confirmmail_custom_Txt2'] = 'Texto da página de confirmação - Confirmação rejeitada';
+$lang['UAM_empty_pwd'] = '[senha em branco]';
+$lang['UAM_group "%s" updated'] = 'Para um melhor desempenho, o grupo %s foi automaticamente definido como o grupo padrão.';
+$lang['UAM_infos_mail %s'] = '%s, por favor encontrar aqui as suas informações de conexão na galeria:';
+$lang['UAM_mail_exclusion_error'] = 'A opção do Piwigo "O endereço de email é obrigatório para todos os usuários", foi desativado enquanto opção plug-in "Exclusão de domínios de correio" do UserAdvancedManager (UAM) manteve-se ativo. Esta combinação é ilógica e não é compatível! <br>
+Por favor, reative a opção do Piwigo "O endereço de e-mail é obrigatório para todos os usuários" para poder, em seguida, desativar a opção "Exclusão de domínios de correio" da UAM se é isso que você quer. <br>
+Nota:. Esta mensagem vai desaparecer depois de ter feito a correção necessária e recarregar a página de administração <br>';
+$lang['UAM_mail_exclusionlist_error'] = 'Atenção! Você entrou em uma nova linha (CR-LF) no início da lista de exclusão de e-mail (mostrado em vermelho abaixo). Embora esta nova linha não seja visível, ele ainda está presente e pode causar o mau funcionamento do plugin. Por favor, re-digite sua lista de exclusão de uma maneira que não começa com uma nova linha.';
+$lang['UAM_no_update_pwd'] = '[Perfil atualizado sem alteração de senha]';
+$lang['UAM_reg_err_login2'] = 'Nome de usuário não tem que coincidir com os seguintes caracteres:';
+$lang['UAM_reg_err_login3'] = 'Segurança: Senha é obrigatório!';
+$lang['UAM_reg_err_login4_%s'] = 'Segurança: Um sistema de controle calcula uma pontuação na complexidade das senhas escolhidas. A complexidade de sua senha é muito baixa (pontuação=%s). Por favor, escolha uma nova senha mais segura, seguindo as seguintes regras: <br>
+- Use letras e números <br>
+- Use maiúsculas e minúsculas<br>
+- Aumentar o comprimento (número de caracteres) <br>
+A pontuação mínima exigido nas senhas pelo administrador é:';
+$lang['UAM_reg_err_login5'] = 'Seu provedor de e-mail está proibido de inscrição. Os Provedores de email proibidos são:';
+$lang['UAM_save_config'] = 'Configurações salvas';
+$lang['UAM_submit'] = 'Submeter configurações';
+$lang['UAM_Demotion of %s'] = 'Rebaixamento de %s';
+$lang['UAM_DumpTxt'] = 'Backup e restauração da configuração';
+$lang['UAM_Dump_Download'] = 'Para baixar o arquivo de backup, por favor marque esta caixa:';
+$lang['UAM_Confirm_Group'] = 'Grupos<br>(deixe ------- para não afetar grupo)';
+$lang['UAM_Confirm_Level'] = 'Nível de privacidade<br>(dexe------- para manter o padrão do Piwigo)';
+$lang['UAM_Confirm_Status'] = 'Estado<br>(dexe------- para manter o padrão do Piwigo)';
+$lang['UAM_Confirm_grpstat_notice'] = 'Atenção: É aconselhável a utilização de um ou outro grupo ou dos estatutos de confirmação e não os dois simultaneamente. O nível de uso pode ser utilizado em conjunto com um grupo ou uma atribuição de estado.';
+$lang['UAM_Default user cannot be pwgreset'] = 'Renovação de senha não pode ser definido para conta de usuário padrão!';
+$lang['UAM_Default_AdminValidationMail_Subject'] = '[username] - Inscrições validada em [mygallery]';
+$lang['UAM_Default_GhstDemotion_Txt'] = 'Desculpe [username], sua conta foi reprovada devido a um  longo período de tempo passado desde a sua última visita a [mygallery]. Por favor, use o seguinte link para confirmar sua conta:';
+$lang['UAM_Default_GhstReminder_Txt'] = 'Olá [username].
+
+Este é um lembrete, porque muito tempo passou desde a sua última visita em nossa galeria [mygallery]. Se você não quer mais usar sua conta de acesso, por favor, avise-nos por respondendo a este e-mail. Sua conta será excluída.
+
+Ao receber esta mensagem e nenhuma nova visita no período de [days] dias, seremos obrigados a excluir sua conta automaticamente.
+
+Atenciosamente,
+
+O administrador da galeria [mygallery] - [MyURL]';
+$lang['UAM_Default_ConfirmMail_Subject'] = '[username] registro de confirmação na [mygallery]';
+$lang['UAM_Default_ConfirmMail_Txt'] = 'Olá [username]! Obrigado a ter se registro na [mygallery].
+
+Para concluir o processo e ser capaz de navegar através das imagens, por favor confirme sua inscrição clicando no link na mensagem.';
+$lang['UAM_Default_GTAutoMail_Subject'] = '[username] - Sua conta foi desclassificada em [mygallery]';
+$lang['UAM_Default_GTReminder_Subject'] = '[username] - Um longo tempo já passou desde a sua última visita em [mygallery]';
+$lang['UAM_Default_GhstDeletion_Txt'] = 'Desculpe [username], sua conta foi excluída devido a um longo período de tempo passado desde a sua última visita a [mygallery]. Por favor, registre novamente.';
+$lang['UAM_Default_CfmMail_Remail_Txt1'] = 'Olá [username].
+
+Esta é uma mensagem de lembrete porque você se registrou em nossa galeria [mygallery], mas você não confirmou sua inscrição e sua chave de confirmação expirou. Ainda é permitido que você acesse a nossa galeria, o período de confirmação foi redefinido. Você tem novamente x dias para confirmar sua inscrição.
+
+Nota: Após este período, a sua conta será excluída permanentemente.';
+$lang['UAM_Default_CfmMail_Remail_Txt2'] = 'Olá [username].
+Esta é uma mensagem de lembrete, porque você se registrou em nossa galeria [mygallery], mas você não confirmou sua inscrição e sua chave de confirmação irá expirar. Para permitir você acessar a nossa galeria, você tem x dias para confirmar sua inscrição clicando no link na mensagem que você deve ter recebido quando se registrou.
+
+Nota: Após este período, a sua conta será excluída permanentemente.';
+$lang['UAM_Default_AdminValidation_Txt'] = 'Olá [username]! Obrigado por se registrar e receber a [mygallery].
+
+Sua conta foi confirmada manualmente pelo webmaster de [mygallery]. Agora você pode entrar em [MyURL] e fazer as mudanças necessárias para o seu perfil.';
+$lang['UAM_Default_CfmMail_Custom_Txt1'] = '[username] conta validada com sucesso!';
+$lang['UAM_Default_CfmMail_Custom_Txt2'] = 'Chave de ativação incorreta/expirada ou conta já validada.';
+$lang['UAM_Err_UserManager_Settings'] = 'Esta página está disponível apenas se "Confirmação de inscrição" estiver ativa e se um grupo de visitantes não confirmados está configurado em "Configurar as confirmações de inscrição".';
+$lang['UAM_Default_InfoMail_Subject'] = '[username] - Suas informações de registro em [mygallery]';
+$lang['UAM_Default_InfoMail_Txt'] = 'Olá [username]!
+Você fez mudanças significativas em seu perfil na galeria [mygallery]. Por favor, Veja abaixo as novas configurações:';
+$lang['UAM_Default_PwdRequest_Txt'] = 'Você pediu uma redefinição de senha em nossa galeria. Por favor, Veja abaixo as novas configurações de conexão.';
+$lang['UAM_Default_RejectConnexion_Txt'] = 'Desculpe, seu registro não é validado ainda, então você não pode se conectar a galeria agora.';
+$lang['UAM_Default_ValidationTimeout_Txt'] = 'Desculpe, sua conta foi excluída por não ter confirmado a sua inscrição no tempo solicitado ([Kdays] dias). Por favor, tente novamente registro com uma conta de e-mail válido e não bloqueado.';
+$lang['UAM_Err_GhostTracker_Settings'] = 'Esta página está disponível apenas se "Ghost Tracker" estiver ativa em "Configurar as inscrições seguidas e outras opções."';
+$lang['UAM_Err_Userlist_Settings'] = 'Esta página está disponível apenas se "Monitoramento de usuários registrados" está ativo no "Definindo as inscrições seguidas e outras opções."';
+$lang['UAM_Err_audit_advise'] = '<b>você tem que executar correções para cumprir com as novas regras que você ativou.<b>Use um utilitário de gerenciamento de banco de dados de contas de usuário corretas diretamente na tabela ###_USERS';
+$lang['UAM_Err_audit_email_forbidden'] = '<b>Esta conta usa um provedor de e-mail proibida:</b>';
+$lang['UAM_Err_audit_username_char'] = '<b>Esta conta usa um ou mais caracteres proibidos:</b>';
 ?>
