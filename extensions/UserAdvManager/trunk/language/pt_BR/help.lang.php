7a6cd98e916e12d93ea7fc5eb354677609a293bf
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-09-14 18:33:44 +0000

    [useradvmanager] Update pt_BR, thanks to : msakik

---
+++ extensions/UserAdvManager/trunk/language/pt_BR/help.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_tipsTitle_d'] = 'Dicas e vários exemplos de uso';
+$lang['UAM_remailtxt2Title'] = 'Email de lembrete sem uma nova chave';
+$lang['UAM_remailtxt1Title'] = 'Email de lembrete com uma nova chave gerada';
+$lang['UAM_passwtestTitle_d'] = 'Digite a senha a ser testada e clique em  &quot;Cálculo do score&quot; para ver o resultado.';
+$lang['UAM_passwtestTitle'] = 'Testando a complexidade da senha';
+$lang['UAM_confirmgrpTitle'] = 'Grupos';
+$lang['UAM_confirmlevelTitle'] = 'Nível de privacidade';
+$lang['UAM_GT_MainTitle_d'] = '- Gerenciamento automático ou manual de usuários fantasmas<br>
+- E-mailing<br>
+...';
+$lang['UAM_GTAutoDelTitle'] = 'Mensagem personalizada para remover uma conta';
+$lang['UAM_AdminValidationMail_d'] = 'Quando um administrador ou Webmaster da galeira confirma manualmente um registro pendente, um email de notificação é enviado ao usuário automaticamente. Coloque aqui o texto personalizado do email de validação.
+<br><br>
+Depois personalize o conteúdo inserindo tags especiais:<br>
+- <b style=&quot;color: red;&quot;>[username]</b> para inserir automaticamente o nome do destinatário do email.<br>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> para inserir o título da galeria.<br>
+- <b style=&quot;color: red;&quot;>[myurl]</b> para inserir a URL da sua galeria, se configurado nas opções do Piwigo.
+<br><br>
+Para usar múltiplos idiomas, é possível utilizar as tags do plugin de Descrições Estendidas, se estiver disponível.';
+$lang['UAM_AdminValidationMail_Subject_d'] = 'Quando um administrador ou Webmaster da galeira confirma manualmente um registro pendente, um email de notificação é enviado ao usuário automaticamente. Coloque aqui o assunto personalizado do email de validação.<br>
+Por padrão, o campo possui um texto genérico em Inglês mas é possível mudá-lo e usar tags de tradução [lang=?] do plugin Descrição Estendida, caso esteja instalado.
+<br><br>
+As tags seguintes de customização estão disponíveis sem usar o plugin de Descrição Estendida:<br>
+- <b style=&quot;color: red;&quot;>[username]</b> para inserir o nome do destinatário do email automaticamente.<br>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> para inserir o título da sua galeria.<br>';
+$lang['UAM_AddURL2Mail_d'] = 'Ativando essa opção, a URL de sua galeria será adicionada ao rodapé dos emails gerados pelo plugin, como uma assinatura.';
+?>
