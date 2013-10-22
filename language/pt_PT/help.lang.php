7a6ba24e831417d2c7fe33a8d73f637ef82924c6
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2013-10-22 04:13:59 +0000

    [useradvmanager] Update pt_PT, thanks to : Bridges

--- language/pt_PT/help.lang.php
+++ language/pt_PT/help.lang.php
@@ -21,4 +21,10 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['UAM_AddURL2Mail_d'] = 'Ao ativar esta opção, irá adicionar o URL da sua galeria ao final dos emails gerados pelo plugin, tal como uma assinatura.';
+$lang['UAM_AdminValidationMail_Subject_d'] = 'Quando um administrador ou Webmaster da galeria confirmar manualmente um registo pendente, a notificação por email é enviada automaticamente ao utilizador. Insira aqui o assunto da sua validação por email própria.<br>
+Por defeito, o campo tem um texto genérico em inglês, mas este pode ser mudado. Utilize as tags de tradução [lang =?] da plugin de descrição estendida, se o mesmo estiver instalado.
+<br><br>
+As seguintes tags adicionais de modificação estão disponíveis sem o plugin de Descrição Extendida.<br>
+- <b style=&quot;color: red;&quot;>[username]</b> para automaticamente inserir o nome do utilizador destinatário do email. <br>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> pata inserir o titulo da sua galeria.<br>';
 ?>
