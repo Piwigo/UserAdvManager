858658de486882f1289ba59f1eb4c395018a2140
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2014-01-13 18:57:30 +0000

    [useradvmanager] Update sk_SK, thanks to : JoeKundlak

---
+++ extensions/UserAdvManager/trunk/language/sk_SK/plugin.lang.php
@@ -0,0 +1,78 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_User: %s'] = 'Užívateľ: %s';
+$lang['UAM_Username_Char_false'] = 'Povoliť všetko (predvolené)';
+$lang['UAM_USRAutoMail'] = 'Automatizovaná emailová pripomienka';
+$lang['UAM_USRAutoDel'] = 'Vlastná správa pre zmazaný účet';
+$lang['UAM_MailInfo_Text'] = 'Úprava informačného emailu: ';
+$lang['UAM_Mail_Info'] = 'Informačný email užívateľovi: ';
+$lang['UAM_Mail_with_key'] = 'Pripomienka s kľúčom';
+$lang['UAM_Mail_without_key'] = 'Pripomienka bez kľúča';
+$lang['UAM_Manual_Validation'] = 'Manuálne potvrdenie';
+$lang['UAM_MiscOptions'] = 'Iné rozličné nastavenia';
+$lang['UAM_Nb_Days'] = 'Rozdiel v dňoch';
+$lang['UAM_LastVisit_Date'] = 'Posledná návšteva';
+$lang['UAM_Delete_selected'] = 'Zmazať';
+$lang['UAM_Disable'] = 'Zakázať (predvolené)';
+$lang['UAM_DumpTxt'] = 'Zálohovať a obnoviť vašu konfiguráciu';
+$lang['UAM_Enable'] = 'Povoliť';
+$lang['UAM_Follow this link to access the gallery'] = 'Pre vstup do galérie použite prosím tento odkaz';
+$lang['UAM_GTAuto'] = 'Automatická správa fantómových užívateľov';
+$lang['UAM_GTAutoDel'] = 'Automatické zmazanie účtov';
+$lang['UAM_NewFeature'] = 'Nová vlastnosť!';
+$lang['UAM_No_Backup_File'] = 'Nie je dostupný žiadny súbor zálohy!';
+$lang['UAM_No_Casse'] = 'Užívateľské mená: Rozlišovanie veľkých a malých písmen';
+$lang['UAM_Password: %s'] = 'Heslo: %s';
+$lang['UAM_Registration_Date'] = 'Dátum registrácie';
+$lang['UAM_Tracking confirmations'] = 'Potvrdenie sledovania';
+$lang['UAM_Tracking registered users'] = 'Sledovanie registrovaných užívateľov';
+$lang['UAM_Title4'] = 'Tipy a príklady použitia';
+$lang['UAM_Title5'] = 'Rozličné nastavenia';
+$lang['UAM_Stuffs_Title'] = 'UAM blok';
+$lang['UAM_SubTitle1'] = 'Nastavenie modulov';
+$lang['UAM_Title1'] = 'Obmedzenia pre registráciu';
+$lang['UAM_Title2'] = 'Potvrdenia registrácie';
+$lang['UAM_RejectConnexion_Custom_Txt'] = 'Upraviť text odmietnutia';
+$lang['UAM_Reminder'] = 'Emailová pripomienka';
+$lang['UAM_Reminder_Sent_NOK'] = 'NIE';
+$lang['UAM_Reminder_Sent_OK'] = 'ÁNO';
+$lang['UAM_Restore_File'] = 'Obnoviť konfiguráciu';
+$lang['UAM_Save'] = 'Spustiť zálohu';
+$lang['UAM_Tab_Global'] = 'Konfigurácia';
+$lang['UAM_no_update_pwd'] = '[profil aktualizovaný bez zmeny hesla]';
+$lang['UAM_Validation of %s'] = 'Potvrdenie pre %s';
+$lang['UAM_ValidationLimit_Info'] = 'Časový limit pre potvrdenie registrácie';
+$lang['UAM_Validated_Status'] = 'Stav pre užívateľov s potvrdenou registráciou<br/>';
+$lang['UAM_Validated_Group'] = 'Skupina pre užívateľov s potvrdenou registráciou<br/>';
+$lang['UAM_Valid_Level'] = 'Úroveň súkromia pre užívateľov s potvrdenou registráciou<br/>';
+$lang['UAM_confirmmail_custom_Txt2'] = 'Text potvrdzovacej stránky - Súhlas odmietnutý';
+$lang['UAM_confirmmail_custom_Txt1'] = 'Text potvrdzovacej stránky - Súhlas akceptovaný';
+$lang['UAM_audit'] = 'Nastavenia Auditu';
+$lang['UAM_audit_ok'] = 'Audit OK';
+$lang['UAM_empty_pwd'] = '[prázdne heslo]';
+$lang['UAM_reg_err_login2'] = 'Užívateľské meno nemôže obsahovať nasledovné znaky: ';
+$lang['UAM_reg_err_login5'] = 'Vaša emailová služba nemá povolenie registrovať sa. Zakázané emailové služby sú: ';
+$lang['UAM_save_config'] = 'Konfigurácia uložená.';
+$lang['UAM_submit'] = 'Odoslať nastavenia';
+$lang['personal_content'] = 'Obsah';
+?>
