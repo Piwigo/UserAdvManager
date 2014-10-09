c86a908438227018282b2382b38bce15ebbe74f8
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-10-09 19:41:39 +0000

    [useradvmanager] Update nl_NL, thanks to : Ellin-E

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,74 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_Default_GhstDemotion_Txt'] = 'Sorry [gebruikersnaam], je account is afgeschaft, omdat je laatste bezoek aan [mygallery] te lang geleden is. We verzoeken je om de onderstaande link te gebruiken om je account te herbevestigen.';
+$lang['UAM_Default_GhstDeletion_Txt'] = 'Sorry [gebruikersnaam], je account is verwijderd omdat je laatste bezoek aan [mygallery] te lang geleden is. We verzoeken je om opnieuw te registreren.';
+$lang['UAM_Default_GTReminder_Subject'] = '[gebruikersnaam] - Het is lang geleden dat je [mygallery] hebt bezocht';
+$lang['UAM_Default_GTAutoMail_Subject'] = '[gebruikersnaam] - Je account is teruggezet naar een lager nuiveau op [mygallery]';
+$lang['UAM_Default_ConfirmMail_Txt'] = 'Hallo [gebruikersnaam]! Bedankt voor je registratie op [mygallery].
+
+Met een klik op de link in dit bericht maak je je registratie compleet en kan je door de afbeeldingen bladeren.';
+$lang['UAM_Default_ConfirmMail_Subject'] = '[gebruikersnaam] registratie bevestiging ingeschakeld op [mygallery]';
+$lang['UAM_Default_ConfirmMail_Remail_Subject'] = 'Herinnering - [gebruikersnaam]registratie bevestiging ingeschakeld op [mygallery]';
+$lang['UAM_Default_CfmMail_Remail_Txt2'] = 'Hallo [gebruikersnaam].
+
+Dit is een bericht ter herinnering: je hebt je geregistreerd op onze galerie [mygallery], maar je hebt je registratie niet bevestigd en je bevestigingssleutel gaat verlopen. Om toegang te hebben tot onze galerie heb je x dagen om je registratie te bevestigen door op de link te klikken in het bericht dat je hebt ontvangen toen je je registreerde.
+
+NB: Na deze periode zal je account blijvend worden verwijderd.';
+$lang['UAM_Default_CfmMail_Remail_Txt1'] = 'Hallo [gebruikersnaam].
+
+Dit is een bericht ter herinnering: je hebt je geregistreerd op onze galerie [mygallery], maar je hebt je registratie niet bevestigd en je bevestigingssleutel is verlopen. Om je toch toegang te geven in onze galerie is je bevestigingsperiode opnieuw ingesteld.
+Je hebt opnieuw x dagen om je registratie te bevestigen.
+
+NB: Na deze periode zal je account blijvend worden verwijderd.';
+$lang['UAM_Default_CfmMail_Custom_Txt2'] = 'Onjuiste / verlopen activatiesleutel of het account is reeds gevalideerd.';
+$lang['UAM_Default_CfmMail_Custom_Txt1'] = '[gebruikersnaam] account is succesvol gevalideerd!';
+$lang['UAM_Default_AdminValidation_Txt'] = 'Hallo [gebruikersnaam]! Bedankt voor je registratie en welkom op [mygallery].';
+$lang['UAM_Default_AdminValidationMail_Subject'] = '[gebruikersnaam] - Registratie gevalideerd ingeschakeld [mygallery]';
+$lang['UAM_Customize_messagesandmails'] = 'Aanpassing van berichten en emails';
+$lang['UAM_CustomPasswRetr'] = 'Aanpassing van de email tbv een verloren gegaan wachtwoord';
+$lang['UAM_Confirm_grpstat_notice'] = 'Let op: Het wordt aangeraden om ofwel de groep ofwel de status bevestiging te gebruiken. Gebruik ze niet tegelijkertijd. Het privacy niveau kan in combinatie met een groep of status toewijzing worden gebruikt.';
+$lang['UAM_Confirm_Status'] = 'Status <b style="color: red;">Waarschuwing: Maak geen gebruik van de "Gast" status. Dit is zinloos en wordt in toekomstige versies verwijderd</b><br/>(verlaat ------- en behoud de standaard van Piwigo)';
+$lang['UAM_Confirm_Mail_true'] = 'Activeer - Bevestiging door de gebruiker';
+$lang['UAM_Confirm_Mail_local'] = 'Activeer - Bevestiging door de beheerder';
+$lang['UAM_Confirm_Group'] = 'Groepen<br/>(verlaat ------- brengt geen veranderingen aan de groep)';
+$lang['UAM_Confirm_Level'] = 'Privacy niveau<br/>(verlaat ------- behoud Piwigo\'s standaard)';
+$lang['UAM_Confirm_Mail'] = 'Registratie bevestiging:';
+$lang['UAM_ConfirmMail_TimeOut_true'] = 'Activeer.Het aantal dagen tot vervaldatum: ';
+$lang['UAM_ConfirmMail_Text'] = 'Aanpassing van de bevestigings-email:';
+$lang['UAM_ConfirmMail_Subject'] = 'Aanpassing van de registratie bevestingings-email';
+$lang['UAM_ConfirmMail_Remail'] = 'Herinnering mbt onbevestigde gebruikers';
+$lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Aanpassing van het herinneringsbericht<b><u>zonder</u></b>aanmaken van de bevestigingssleutel';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Aanpassing van het herinneringsbericht<b><u>zonder</u></b>opnieuw aanmaken van de bevestigingssleutel';
+$lang['UAM_ConfirmMail_ReMail_Subject'] = 'Aanpassing van de registratie bevestiging herinnerings-email';
+$lang['UAM_Change'] = 'Verander het groeps/status/privacy niveau van de bevestigde gebruikers';
+$lang['UAM_Bad_version_backup'] = 'De versie van het backup bestand klopt niet met de versie van de plugin! Er heeft geen herstel plaatsgevonden.';
+$lang['UAM_AdminValidationMail_Text'] = 'Berichtgeving van een handmatige registratie bevestiging';
+$lang['UAM_AdminValidationMail_Subject'] = 'Handmatige aanpassing van de email-validatie';
+$lang['UAM_AdminConfMail'] = 'Registratie bevestiging voor beheerders';
+$lang['UAM_AddURL2Mail'] = 'Voeg de URL van de galerie toe aan het einde van de emails (als handtekening)';
+$lang['UAM_%d_Validated_Users'] = '%d Gebruikers zijn handmatig bevestigd';
+$lang['UAM_%d_Validated_User'] = '%d Gebruiker is handmatig bevestigd';
+$lang['UAM_%d_Reminders_Sent'] = '%d herinneringsberichten zijn verzonden';
+$lang['UAM_%d_Reminder_Sent'] = '%d herinneringsbericht is verzonden';
+$lang['UAM_%d_Mails_With_Key'] = '%d berichten met vernieuwing van de sleutel zijn verzonden';
+$lang['UAM_%d_Mail_With_Key'] = '%d bericht met vernieuwing van de sleutel is verzonden';
