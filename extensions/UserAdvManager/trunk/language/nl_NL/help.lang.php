0073fc75006abc2f69c775859a2f78df4e3de075
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2014-10-16 20:01:47 +0000

    [useradvmanager] Update nl_NL, thanks to : Ellin-E

--- extensions/UserAdvManager/trunk/language/nl_NL/help.lang.php
+++ extensions/UserAdvManager/trunk/language/nl_NL/help.lang.php
@@ -233,7 +233,7 @@ Vervolgens kan je de inhoud aanpassen met speciaal ingevoegde labels:<br/>
 - <b style=&quot;color: red;&quot;>[mygallery]</b> het invoegen van de titel van je galerie.<br/>
 - <b style=&quot;color: red;&quot;>[myurl]</b> het invoegen van de URL van je galerie, mits je deze hebt ingesteld in Piwigo\'s instellingen.<br/>
 <br/><br/>
-Voor meerdere tal kan je de labels van de plugin Extended Description gebruiken als deze is geactiveerd.';
+Voor meerdere talen kan je de labels van de plugin Extended Description gebruiken als deze is geactiveerd.';
 $lang['UAM_gttextTitle_d'] = 'Voeg je eigen tekst in de herinneringsemail om de gebruiker aan te sporen de galerie weer te bezoeken (NB. De vooraf ingevulde tekst is een voorbeeld).
 <br/><br/>
 Vervolgens kan je de inhoud aanpassen met speciaal ingevoegde labels:<br/>
@@ -242,7 +242,7 @@ Vervolgens kan je de inhoud aanpassen met speciaal ingevoegde labels:<br/>
 - <b style=&quot;color: red;&quot;>[myurl]</b> het invoegen van de URL van je galerie, mits je deze hebt ingesteld in Piwigo\'s instellingen.<br/>
 - <b style=&quot;color: red;&quot;>[dagen]</b> het invoegen van het maximaal aantal dagen tussen 2 bezoeken.
 <br/><br/>
-Voor meerdere tal kan je de labels van de plugin Extended Description gebruiken als deze is geactiveerd.';
+Voor meerdere talen kan je de labels van de plugin Extended Description gebruiken als deze is geactiveerd.';
 $lang['UAM_gtTitle_d'] = 'Als Ghost Tracker is ingeschakeld en geïnitialiseerd, dan vind je hier een lijst met geregistreerde bezoekers die sinds x dagen niet zijn terug geweest. &quot;x&quot; is het aantal dagen dat is ingesteld in het tabblad Algemene Instellingen. Bovendien zie je een kolom waarin staat of er een herinneringsemail is verzonden naar deze bezoekers. Je ziet dus direct de bezoekers die geen aandacht hebben gegeven aan de herinnering.<br/><br/>In dit scherm kan je:
 <br/><br/>
 - Handmatig accounts verwijderen<b>(handmatige verwijdering)</b>
@@ -250,3 +250,42 @@ $lang['UAM_gtTitle_d'] = 'Als Ghost Tracker is ingeschakeld en geïnitialiseerd,
 - Een herinneringsemail genereren<b>met een reset van de laatste bezoekdatum</b>. Hiermee geef je een \'joker\' aan de beoogde bezoeker. Als de bezoeker al een herinneringsemail heeft gehad kan je toch een nieuwe email sturen waarin de datum van het laatste bezoek opnieuw wordt ingesteld.
 <br/><br/>
 <b>Tabel Sorteerfunctie</b>:Je kan de data sorteren door op de kolom-kop te klikken. Houdt de SHIFT-toets vast om tot 4 gelijktijdige kolommen te sorteren. ';
+$lang['UAM_userlistTitle_d'] = 'Dit is een informatiepagina voor de beheerder. Het toont een lijst met alle geregistreerde gebruikers naar datum en het aantal dagen sinds hun laatste bezoek. De lijst is gesorteerd in het oplopende aantal dagen.
+<br/><br/>
+<b><u>Alleen bij een ingeschakelde Ghost Tracker</u></b> verschijnt het aantal niet bezochte dagen in de onderstaande code, afhankelijk van het maximum dat je hebt ingesteld in de Ghost Tracker instellingen:
+<br/>
+- <b style=&quot;color: lime;&quot;>Groen</b> : Als de bezoeker de galerie <b style=&quot;color: lime;&quot;><u>minder dan 50%</u></b> van het maximum heeft bezocht,  zoals aangegeven in de Ghost Tracker.<br/>
+- <b style=&quot;color: orange;&quot;>Oranje</b> : Als de bezoeker de galerie <b style=&quot;color: lime;&quot;><u>tussen de 50% en 99%</u></b> van het maximum heeft bezocht, zoals aangegeven in de Ghost Tracker.<br/>
+- <b style=&quot;color: red;&quot;>Rood</b> : Als de bezoeker de galerie <b style=&quot;color: lime;&quot;><u>meer dan 100%</u></b> van het maximum heeft bezocht,  zoals aangegeven in de Ghost Tracker.<br/>. <b><u>In dit geval moet de gebruiker ook in de Ghost Tracker tabel voorkomen.</u></b><br/>
+<br/>
+Voorbeeld:
+<br/>
+In Ghost Tracker is de maximale periode is ingesteld op 100 dagen.
+<br/>
+Een gebruiker verschijnt in het groen als hij de galerie minder dan 50 dagen heeft bezocht, in het oranje als zijn laatste bezoek tussen 50 en 99 dagen plaatsvond en in het rood als hij hier 100 of meer dagen was.
+<br/><br/>
+<b>NOTA BENE</b>: In de lijst staan niet de degenen die hun registratie niet hebben bevestigd (mits de optie voor registratiebevestiging is ingeschakeld). Deze gebruikers worden beheerd op een aparte manier in het tabblad &quot;Controle van Bevestigingen&quot;
+<br/><br/>
+<b>Tabel Sortering</b>: Je kan de data sorteren door op de kolom-koppen te klikken. Als je de SHIFT-toets ingedrukt houdt kan je tot 4 kolommen tegelijk sorteren.';
+$lang['UAM_remailtxt2Title_d'] = 'Voeg je eigen introductietekst in de herinneringsemail zonder de opnieuw gegenereerde bevestigingssleutel.
+<br/><br/>
+Als je dit veld leeg laat zal de herinneringsemail geen inhoud bevatten. Het wordt sterk aanbevolen om een kleine tekstuitleg in te voegen. (NB. De vooraf ingevulde tekst is bedoeld als voorbeeld)
+<br/><br/>
+Vervolgens kan je de inhoud aanpassen met speciaal ingevoegde labels:<br/>
+- <b style=&quot;color: red;&quot;>[gebruikersnaam]</b> het automatisch invoegen van de naam van de gebruiker voor wie de email bestemd is.<br/>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> het invoegen van de titel van je galerie.<br/>
+- <b style=&quot;color: red;&quot;>[myurl]</b> het invoegen van de URL van je galerie, mits je deze hebt ingesteld in Piwigo\'s instellingen.<br/>
+- <b style=&quot;color: red;&quot;>[Kdays]</b> het invoegen van het aantal dagen om de registratie te bevestigen. (&quot;Beperkte Verloopdatum voor registratiebevestiging;&quot; moet zijn ingeschakeld).
+<br/><br/>
+Voor meerdere talen kan je de labels van de plugin Extended Description gebruiken als deze is geactiveerd.';
+$lang['UAM_remailtxt1Title_d'] = 'Voeg je eigen introductietekst in de herinneringsemail met de opnieuw gegenereerde bevestigingssleutel.
+<br/><br/>
+Als je dit veld leeg laat zal de herinneringsemail alleen de bevestigingslink bevatten. Het wordt sterk aanbevolen om een kleine tekstuitleg in te voegen. (NB. De vooraf ingevulde tekst is bedoeld als voorbeeld)
+<br/><br/>
+Vervolgens kan je de inhoud aanpassen met speciaal ingevoegde labels:<br/>
+- <b style=&quot;color: red;&quot;>[gebruikersnaam]</b> het automatisch invoegen van de naam van de gebruiker voor wie de email bestemd is.<br/>
+- <b style=&quot;color: red;&quot;>[mygallery]</b> het invoegen van de titel van je galerie.<br/>
+- <b style=&quot;color: red;&quot;>[myurl]</b> het invoegen van de URL van je galerie, mits je deze hebt ingesteld in Piwigo\'s instellingen.<br/>
+- <b style=&quot;color: red;&quot;>[Kdays]</b> het invoegen van het aantal dagen om de registratie te bevestigen. (&quot;Beperkte Verloopdatum voor registratiebevestiging;&quot; moet zijn ingeschakeld).
+<br/><br/>
+Voor meerdere talen kan je de labels van de plugin Extended Description gebruiken als deze is geactiveerd.';
