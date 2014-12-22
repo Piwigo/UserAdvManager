12448b8eaa3ca8d9454bcf7f97b95333a63810dc
TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-12-22 15:55:29 +0000

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,74 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['UAM_Confirm_Group'] = 'Grupper<br/>(la ------- for ikke å påvirke gruppe)';
+$lang['UAM_Confirm_Level'] = 'Personvernnivå<br/>(la ------- for å beholde Piwigo\'s standard)';
+$lang['UAM_Confirm_Mail'] = 'Bekreftelse på påmelding:';
+$lang['UAM_Confirm_Mail_local'] = 'Aktiver - Bekreftelse av admin';
+$lang['UAM_Confirm_Mail_true'] = 'Aktiver - Bekreftelse av bruker';
+$lang['UAM_Confirm_Status'] = 'Vedtekter <b style ="color: red;">Advarsel: Ikke bruk "Gjest" status. Fordi ubrukelig, denne funksjonen kan bli fjernet i fremtidige versjoner</b><br/>(la ------- for å beholde Piwigo\'s standard)';
+$lang['UAM_Confirm_grpstat_notice'] = 'Forsiktig: Det anbefales å bruke enten gruppen eller vedtekt bekreftelse og ikke begge samtidig. Personvernnivå kan brukes sammen med en gruppe eller status oppdrag.';
+$lang['UAM_CustomPasswRetr'] = 'Tilpass glemt passord e-postinnhold';
+$lang['UAM_Customize_messagesandmails'] = 'Tilpass meldinger og e-post';
+$lang['UAM_Default_AdminValidationMail_Subject'] = '[Brukernavn] - Registrering validert på [mygallery]';
+$lang['UAM_Default_AdminValidation_Txt'] = 'Hei [brukernavn]! Takk for din registrering og velkommen til [mygallery].
+
+Din konto har blitt manuelt bekreftet av webansvarlig for [mygallery]. Du kan nå logge inn på [myurl] og foreta eventuelle nødvendige endringer i profilen din.';
+$lang['UAM_Default_CfmMail_Custom_Txt1'] = '[Brukernavn] konto vellykket validert!';
+$lang['UAM_Default_CfmMail_Custom_Txt2'] = 'Feil / utløpt aktiveringsnøkkel eller konto allerede validert.';
+$lang['UAM_Default_CfmMail_Remail_Txt1'] = 'Hei [brukernavn].
+
+Dette er en påminnelse meldingen fordi du er registrert på vårt galleri [mygallery] men du bekreftet ikke din registrering og aktiveringsnøkkel er dermed utløpt på dato. For å gi deg tilgang til galleriet vårt, har din bekreftelses periode blitt resatt. Du har du x dager for å bekrefte din registrering.
+
+Merk: Etter denne perioden, vil din konto bli slettet.';
+$lang['UAM_Default_CfmMail_Remail_Txt2'] = 'Hei [brukernavn].
+
+Dette er en påminnelse meldingen fordi du er registrert på vårt galleri [mygallery] men du bekreftet ikke din registrering og aktiveringsnøkkel er dermed utløpt på dato. For å gi deg tilgang til galleriet vårt, har du x dager for å bekrefte din registrering ved å klikke på lenken i meldingen du skal ha mottatt da du registrerte deg.
+
+Merk: Etter denne perioden, vil din konto bli slettet.';
+$lang['UAM_Default_ConfirmMail_Remail_Subject'] = 'Påminnelse - [brukernavn] registreringsbekreftelse på [mygallery]';
+$lang['UAM_Default_ConfirmMail_Subject'] = '[Brukernavn] registreringsbekreftelse på [mygallery]';
+$lang['UAM_Default_ConfirmMail_Txt'] = 'Hei [brukernavn]! Takk for å ha registrert deg på [mygallery].
+
+For å fullføre prosessen og kunne bla gjennom bildene, ber vi deg bekrefte din registrering ved å klikke på linken i denne meldingen.';
+$lang['UAM_Default_GTAutoMail_Subject'] = '[Brukernavn] - Din konto har blitt degradert på [mygallery]';
+$lang['UAM_Default_GTReminder_Subject'] = '[Brukernavn] - Lang tid har gått siden ditt siste besøk på [mygallery]';
+$lang['UAM_%d_Mail_With_Key'] = '%d melding med passord fornyelse var sendt';
+$lang['UAM_%d_Mails_With_Key'] = '%d melding med passord fornyelse ble sendt';
+$lang['UAM_%d_Reminder_Sent'] = '%d påminnelse melding var sendt';
+$lang['UAM_%d_Reminders_Sent'] = '%d påminnelse melding ble sendt';
+$lang['UAM_%d_Validated_User'] = '%d Bruker bekreftet manuelt';
+$lang['UAM_%d_Validated_Users'] = '%d Brukere bekreftet manuelt';
+$lang['UAM_AddURL2Mail'] = 'Legg inn nettadressen til galleriet på slutten av e-post (som en signatur)';
+$lang['UAM_AdminConfMail'] = 'Bekreftelse på registrering for admins';
+$lang['UAM_AdminValidationMail_Subject'] = 'Tilpass manuell validering av e-postemne';
+$lang['UAM_AdminValidationMail_Text'] = 'Melding om manuell bekreftelse av registrering';
+$lang['UAM_Admins_Copy'] = 'Kopi av alle e-poster til admins:';
+$lang['UAM_Bad_version_backup'] = 'Den versjonen av sikkerhetskopifilen samsvarer ikke med denne versjonen av programtillegget! Restaureringen ble ikke utført.';
+$lang['UAM_Change'] = 'Endre Gruppe/Status/Privat nivå av brukerne som har blitt bekreftet';
+$lang['UAM_ConfirmMail_ReMail_Subject'] = 'Tilpass registreringsbekreftelse påminnelse e-postemne';
+$lang['UAM_ConfirmMail_ReMail_Txt1'] = 'Tilpasse påminnelse beskjeden <b><u>med</u></b> ny regenerering av bekreftelse nøkkel.';
+$lang['UAM_ConfirmMail_ReMail_Txt2'] = 'Tilpasse påminnelse beskjeden <b><u>uten</u></b> ny regenerering av bekreftelse nøkkel.';
+$lang['UAM_ConfirmMail_Remail'] = 'Påmindelse av ubekreftede brukere';
+$lang['UAM_ConfirmMail_Subject'] = 'Tilpass registreringsbekreftelsens e-postemne';
+$lang['UAM_ConfirmMail_Text'] = 'Tilpasse bekreftelses e-post:';
+$lang['UAM_ConfirmMail_TimeOut_true'] = 'Aktiver. Antall dager før utløp:';
